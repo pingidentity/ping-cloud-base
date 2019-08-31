226d312aebbe49429dd40bc221092f53288254e3
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-08-31 01:48:56 +0000

    Ci test

--- ci-scripts/build/deploy.sh
+++ ci-scripts/build/deploy.sh
@@ -1,38 +1,14 @@
 #!/bin/sh
 set -ex
 
-SCRIPT_HOME=$(cd $(dirname ${0}); pwd)
-. ${SCRIPT_HOME}/../common.sh
-
-# Kubernetes variables
-KUBECONFIG=${CI_PROJECT_DIR}/kubeconfig
-
-# AWS variables
-AWS_REGION=us-west-2
-AWS_ACCOUNT_ID=705370621539
-AWS_ROLE_NAME=CSG
-AWS_ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/${AWS_ROLE_NAME}"
-AWS_CONFIG_DIR="/root/.aws"
-AWS_CONFIG_FILE="${AWS_CONFIG_DIR}/config"
-AWS_CREDENTIALS_FILE="${AWS_CONFIG_DIR}/credentials"
-
-# Create the AWS config file to use the specific IAM role
-mkdir -p "${AWS_CONFIG_DIR}"
-echo '[default]' > "${AWS_CONFIG_FILE}"
-echo 'output = json' >> "${AWS_CONFIG_FILE}"
-echo "region = ${AWS_REGION}" >> "${AWS_CONFIG_FILE}"
-echo '[profile gte]' >> "${AWS_CONFIG_FILE}"
-echo "region = ${AWS_REGION}" >> "${AWS_CONFIG_FILE}"
-echo 'source_profile = default' >> "${AWS_CONFIG_FILE}"
-echo "role_arn = ${AWS_ROLE_ARN}" >> "${AWS_CONFIG_FILE}"
-
-# Create the AWS credentials file to use the specific IAM role
-echo '[default]' > "${AWS_CREDENTIALS_FILE}"
-echo "aws_access_key_id = ${AWS_ACCESS_KEY_ID}" >> "${AWS_CREDENTIALS_FILE}"
-echo "aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" >> "${AWS_CREDENTIALS_FILE}"
-echo "role_arn = ${AWS_ROLE_ARN}" >> "${AWS_CREDENTIALS_FILE}"
-
 # Deploy to Kubernetes
-log "Deploying config under ${CI_PROJECT_DIR}/test"
-cd  ${CI_PROJECT_DIR}/test
-kustomize build | envsubst | kubectl apply -f -
+kustomize build ${CI_PROJECT_DIR}/test | envsubst | kubectl apply -f -
+
+# Give each pod 5 minutes to initialize. The PF, PA apps deploy fast. PD is the
+# long pole.
+for deployment in $(kubectl get deployment,statefulset -n ping-cloud -o name); do
+  kubectl rollout status --timeout 300s ${deployment} -n ping-cloud -w
+done
+
+# Print out the ingress object for the ping stack
+kubectl get ingress -n ping-cloud
