f4def3dc4c0828ceeb8b621e852297ce009cd4e2
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-09-04 12:04:44 -0500

    Commenting out testing the logs URL since elastic stack is deployed just once

--- ci-scripts/test/integration/test-urls.sh
+++ ci-scripts/test/integration/test-urls.sh
@@ -3,4 +3,7 @@
 SCRIPT_HOME=$(cd $(dirname ${0}); pwd)
 . ${SCRIPT_HOME}/../../common.sh
 
-testUrl ${LOGS_CONSOLE}
+# FIXME: since the elastic stack is just deployed once on the shared cluster,
+# this URL will change as soon as a new branch is cut. Uncomment the test once
+# this is fixed.
+# testUrl ${LOGS_CONSOLE}
