c7fd86e859861c3fc38c28687d18effb4553befa
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-09-06 16:33:31 -0500

    Removing a few FIXMEs

--- ci-scripts/test/pingfederate/client-credentials-grant-type.sh
+++ ci-scripts/test/pingfederate/client-credentials-grant-type.sh
@@ -6,7 +6,4 @@ SCRIPT_HOME=$(cd $(dirname ${0}); pwd)
 URL="${PINGFEDERATE_AUTH_ENDPOINT}/as/token.oauth2?grant_type=client_credentials&scope="
 log "Attempting to obtain access token from ${URL}"
 
-# FIXME: We need to tweak the baseline server profile for this to work.
-# Basically, the PingDirectory client must be allowed to request
-# client_credentials grant type.
 curl --silent -v -k -X POST -u 'PingDirectory:2FederateM0re' "${URL}"
