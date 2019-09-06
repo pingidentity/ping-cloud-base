9883d73390d5b0873a7f5f51cfecb79058f0cb5e
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-09-06 13:15:52 -0500

    Enable client credentials grant test now that PingDirectory client allows it

--- ci-scripts/test/pingfederate/client-credentials-grant-type.sh
+++ ci-scripts/test/pingfederate/client-credentials-grant-type.sh
@@ -9,4 +9,4 @@ log "Attempting to obtain access token from ${URL}"
 # FIXME: We need to tweak the baseline server profile for this to work.
 # Basically, the PingDirectory client must be allowed to request
 # client_credentials grant type.
-#curl --silent -v -k -X POST -u 'PingDirectory:2FederateM0re' "${URL}"
+curl --silent -v -k -X POST -u 'PingDirectory:2FederateM0re' "${URL}"
