097a6e952d4a014d6116b52961c96e710d3a9f55
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-09-16 15:08:31 +0000

    Periodic backups and manual restore job

--- dev-env.sh
+++ dev-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 declare dryrun="false"
-#
+
 # Parse Parameters
-#
 while getopts 'n' OPTION
 do
   case ${OPTION} in
@@ -15,11 +15,11 @@ do
       ;;
   esac
 done
-#
-# Show initial values for domain & enviroment
-#
-echo "Initial Value TENANT_DOMAIN: ${TENANT_DOMAIN}"
-echo "Initial Value ENVIRONMENT: ${ENVIRONMENT}"
+
+# Show initial values for domain & environment
+echo "Initial TENANT_DOMAIN: ${TENANT_DOMAIN}"
+echo "Initial ENVIRONMENT: ${ENVIRONMENT}"
+
 # A script that may be used to set up a dev/test environment against the
 # current cluster. Must have the GTE devops user and key exported as
 # environment variables.
@@ -27,22 +27,30 @@ export ENVIRONMENT=-"${ENVIRONMENT:-${USER}}"
 export TENANT_DOMAIN="${TENANT_DOMAIN:-eks-poc.au1.ping-lab.cloud}"
 
 ENVIRONMENT_NO_HYPHEN_PREFIX=$(echo ${ENVIRONMENT/#-})
+
+echo "Using TENANT_DOMAIN: ${TENANT_DOMAIN}"
+echo "Using ENVIRONMENT: ${ENVIRONMENT_NO_HYPHEN_PREFIX}"
+
 NAMESPACE=ping-cloud-${ENVIRONMENT_NO_HYPHEN_PREFIX}
 DEPLOY_FILE=/tmp/deploy.yaml
 
-kustomize build test | envsubst > ${DEPLOY_FILE}
+kustomize build test |
+  envsubst '${PING_IDENTITY_DEVOPS_USER}
+    ${PING_IDENTITY_DEVOPS_KEY}
+    ${ENVIRONMENT}
+    ${TENANT_DOMAIN}' > ${DEPLOY_FILE}
 sed -i.bak -E "s/((namespace|name): )ping-cloud$/\1${NAMESPACE}/g" ${DEPLOY_FILE}
 
-if [ "${dryrun}" = 'false' ]; then
-   echo "Deploying ${DEPLOY_FILE} to namespace ${NAMESPACE} for tenant ${TENANT_DOMAIN}"
-   kubectl apply -f ${DEPLOY_FILE}
+if [[ "${dryrun}" = 'false' ]]; then
+  echo "Deploying ${DEPLOY_FILE} to namespace ${NAMESPACE} for tenant ${TENANT_DOMAIN}"
+  kubectl apply -f ${DEPLOY_FILE}
 
-   # Print out the ingress objects for logs and the ping stack
-   kubectl get ingress -n elastic-stack-logging
-   kubectl get ingress -n ${NAMESPACE}
+  # Print out the ingress objects for logs and the ping stack
+  kubectl get ingress -n elastic-stack-logging
+  kubectl get ingress -n ${NAMESPACE}
 
-   # Print out the  pods for the ping stack
-   kubectl get pods -n ${NAMESPACE}
+  # Print out the  pods for the ping stack
+  kubectl get pods -n ${NAMESPACE}
 else
-   less "${DEPLOY_FILE}"
+  less "${DEPLOY_FILE}"
 fi
