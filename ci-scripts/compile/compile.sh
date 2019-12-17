b3e66feea57f0418789cf07252dc7cc513ae5114
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2019-12-17 19:28:26 +0000

    Fixed the kustomization small/medium environment to explicitly choose clustered PF/PA

--- ci-scripts/compile/compile.sh
+++ ci-scripts/compile/compile.sh
@@ -4,8 +4,13 @@ SCRIPT_HOME=$(cd $(dirname ${0}); pwd)
 . ${SCRIPT_HOME}/../common.sh
 
 SCRIPTS=$(find ${SCRIPT_HOME} -type f -name '*-kustomizations.sh')
+STATUS=0
 
 for SCRIPT in ${SCRIPTS}; do
   log "Running script ${SCRIPT}"
   ${SCRIPT}
+  RESULT=${?}
+  test ${STATUS} -eq 0 && STATUS=${RESULT}
 done
+
+exit ${STATUS}
