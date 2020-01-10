99274055af2cc051ff560b00f2be95b59d84d106
Calvin Carter <calvincarter@pingidentity.com>
2020-01-10 21:15:14 +0000

    add pingacess-cluster label to admin and engines, change pf to pingfederate, and pd ds to pingdirectory

--- ci-scripts/test/pingdirectory/02-csd-upload.sh
+++ ci-scripts/test/pingdirectory/02-csd-upload.sh
@@ -4,7 +4,7 @@ SCRIPT_HOME=$(cd $(dirname ${0}); pwd)
 . ${SCRIPT_HOME}/../../common.sh
 
 expected_files() {
-  kubectl logs $(kubectl get pod -o name | grep ds-csd-upload | cut -d/ -f2) |
+  kubectl logs $(kubectl get pod -o name | grep pingdirectory-csd-upload | cut -d/ -f2) |
     tail -1 |
     tr ' ' "\n"
 }
@@ -22,7 +22,7 @@ log "Applying the CSD upload job"
 kubectl apply -f "${UPLOAD_JOB}" -n "${NAMESPACE}"
 
 log "Waiting for CSD upload job to complete"
-kubectl wait --for=condition=complete --timeout=300s job/ds-csd-upload -n "${NAMESPACE}"
+kubectl wait --for=condition=complete --timeout=300s job/pingdirectory-csd-upload -n "${NAMESPACE}"
 
 log "Expected CSD files:"
 expected_files | tee /tmp/expected.txt
