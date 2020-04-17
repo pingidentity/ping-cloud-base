e7ee4a3b0df0df8678ac48bc3e4973502a983a3d
Chris Schaffer <chrisschaffer@pingidentity.com>
2020-04-17 14:30:57 +0000

    [skip pipeline] add logic to check for new and old password before changing password and add marker file after restore script

--- profiles/aws/pingaccess/hooks/50-before-post-start.sh
+++ profiles/aws/pingaccess/hooks/50-before-post-start.sh
@@ -5,5 +5,8 @@
 echo "OPERATIONAL_MODE:"${OPERATIONAL_MODE}
 
 if test ! -z "${OPERATIONAL_MODE}" && test "${OPERATIONAL_MODE}" = "CLUSTERED_ENGINE"; then
-  run_hook "51-add-engine.sh"
+  sh "${HOOKS_DIR}/51-add-engine.sh"
+  if test $? -ne 0; then
+    exit 1
+  fi
 fi
