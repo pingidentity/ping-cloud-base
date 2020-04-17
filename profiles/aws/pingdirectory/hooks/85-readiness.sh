45a20a0d98bdd48421f7136d52fd9857f566f990
calvincarter-ping <calvincarter@pingidentity.com>
2020-04-17 17:01:33 -0500

    [skip pipeline] set rediness to perform healthcheck on baseDN platformconfig

--- profiles/aws/pingdirectory/hooks/85-readiness.sh
+++ profiles/aws/pingdirectory/hooks/85-readiness.sh
@@ -1,8 +1,32 @@
 #!/usr/bin/env sh
 
-# Verify that server is responsive on its LDAP secure port
-echo "readiness: verifying root DSE access"
-/opt/liveness.sh || exit 1
+########################################################################################################################
+# Verify that the provided BASE_DN responds to a search request over LDAP.
+#
+# Arguments
+#   ${1} -> The base DN for the search request.
+########################################################################################################################
+health_check() {
+  BASE_DN="${1}"
+  ldapsearch \
+    --dontWrap \
+    --terse \
+    --suppressPropertiesFileComment \
+    --noPropertiesFile \
+    --operationPurpose "Docker container liveness check" \
+    --port "${LDAPS_PORT}" \
+    --useSSL \
+    --trustAll \
+    --baseDN "${BASE_DN}" \
+    --searchScope base "(&)" 1.1 \
+    2>/dev/null
+  HEALTH_STATUS=${?}
+  echo "Health check for ${BASE_DN} status: ${HEALTH_STATUS}"
+  return ${HEALTH_STATUS}
+}
+# Verify that server is responsive on its heartbeat endpoint
+echo "readiness: verifying heartbeat endpoint is accessible"
+health_check "o=platformconfig" || exit 1
 
 # Verify that post-start initialization is complete on this host
 echo "readiness: verifying that post-start initialization is complete on ${HOSTNAME}"
