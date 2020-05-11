0931402f2fd0100cacabfcc76521a0929833d074
Suraj Thapa <surajthapa@pingidentity.com>
2020-05-11 13:21:47 +1000

    [skip pipeline] PF test archive-data

--- profiles/aws/pingfederate/hooks/82-upload-archive-data-s3.sh
+++ profiles/aws/pingfederate/hooks/82-upload-archive-data-s3.sh
@@ -11,57 +11,45 @@ test -f "${STAGING_DIR}/env_vars" && . "${STAGING_DIR}/env_vars"
 test ! -z "${1}" && BACKUP_URL="${1}"
 echo "Uploading to location ${BACKUP_URL}"
 
-# Install AWS CLI if the upload location is S3
-if test "${BACKUP_URL#s3}" == "${BACKUP_URL}"; then
-   echo "Upload location is not S3"
-   exit 1
-else
-   installTools
-fi
+# Set required environment variables for skbn
+initializeSkbnConfiguration
 
 # Create and export archive data into file data.mm-dd-YYYY.HH.MM.SS.zip
-DST_FILE="data-`date +%m-%d-%Y.%H.%M.%S`.zip"
+DST_FILE_TIMESTAMP="data-`date +%m-%d-%Y.%H.%M.%S`.zip"
 DST_DIRECTORY="/tmp/k8s-s3-upload-archive"
 mkdir -p ${DST_DIRECTORY}
 
 # Make request to admin API and export latest data
 make_api_request -X GET \
   https://localhost:${PINGFEDERATE_ADMIN_PORT}/pf-admin-api/v1/configArchive/export \
-  -o ${DST_DIRECTORY}/${DST_FILE}
+  -o ${DST_DIRECTORY}/${DST_FILE_TIMESTAMP}
 
 # Validate admin API call was successful and that zip isn't corrupted
-if test ! $? -eq 0 || test "$( unzip -t ${DST_DIRECTORY}/${DST_FILE} > /dev/null 2>&1;echo $?)" != "0" ; then
+if test ! $? -eq 0 || test "$( unzip -t ${DST_DIRECTORY}/${DST_FILE_TIMESTAMP} > /dev/null 2>&1;echo $?)" != "0" ; then
   echo "Failed to export archive"
   # Cleanup k8s-s3-upload-archive temp directory
   rm -rf ${DST_DIRECTORY}
   exit 1
 fi
 
-BUCKET_URL_NO_PROTOCOL=${BACKUP_URL#s3://}
-BUCKET_NAME=$(echo ${BUCKET_URL_NO_PROTOCOL} | cut -d/ -f1)
-DIRECTORY_NAME=$(echo ${PING_PRODUCT} | tr '[:upper:]' '[:lower:]')
+# Two copy of the backup will be pushed to cloud storage.
+# Make a copy: latest.zip
+DST_FILE_LATEST=latest.zip
+cp "${DST_DIRECTORY}/$DST_FILE_TIMESTAMP" "${DST_DIRECTORY}/$DST_FILE_LATEST" 
 
-echo "Creating directory ${DIRECTORY_NAME} under bucket ${BUCKET_NAME}"
-aws s3api put-object --bucket "${BUCKET_NAME}" --key "${DIRECTORY_NAME}"/
+echo "Copying: '${DST_FILE_LATEST}' to '${SKBN_CLOUD_PREFIX}/${DST_FILE_LATEST}'"
 
-if test "${BACKUP_URL}" == */pingfederate; then
-  TARGET_URL="${BACKUP_URL}"
-else
-  TARGET_URL="${BACKUP_URL}/${DIRECTORY_NAME}"
-fi
-
-aws s3 cp "${DST_DIRECTORY}/${DST_FILE}" "${TARGET_URL}/"
-AWS_API_RESULT="${?}"
-
-echo "Upload return code: ${AWS_API_RESULT}"
-
-if [ "${AWS_API_RESULT}" != "0" ]; then
-  echo "Upload was unsuccessful - crash the container"
+if ! skbnCopy "${SKBN_K8S_PREFIX}/${DST_DIRECTORY}/${DST_FILE_LATEST}" \
+  "${SKBN_CLOUD_PREFIX}/${DST_FILE_LATEST}"; then
   exit 1
 fi
 
-# Print the filename of the uploaded file to s3
-echo "Uploaded file name: ${DST_FILE}"
+echo "Copying: '${DST_FILE_TIMESTAMP}' to '${SKBN_CLOUD_PREFIX}/${DST_FILE_TIMESTAMP}'"
+
+if ! skbnCopy "${SKBN_K8S_PREFIX}/${DST_DIRECTORY}/${DST_FILE_TIMESTAMP}" \
+  "${SKBN_CLOUD_PREFIX}/${DST_FILE_TIMESTAMP}"; then
+  exit 1
+fi
 
 # Print listed files from k8s-s3-upload-archive
 ls ${DST_DIRECTORY}
