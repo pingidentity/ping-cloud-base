371f3a75c0e6dde1b97afcb51159b3904a8c635a
Suraj Thapa <surajthapa@pingidentity.com>
2020-05-12 20:24:02 +0000

    PDO-944 - Updates for PD to use skbn to move file from and to k8s/cloud storage.

--- profiles/aws/pingdirectory/hooks/10-download-artifact.sh
+++ profiles/aws/pingdirectory/hooks/10-download-artifact.sh
@@ -26,11 +26,6 @@ if test -f "${STAGING_DIR}/artifacts/artifact-list.json"; then
       echo ${ARTIFACT_LIST_JSON} | jq
       if test $(echo $?) == "0"; then
 
-        # Install AWS CLI if the upload location is S3
-        if test ! "${ARTIFACT_REPO_URL#s3}" == "${ARTIFACT_REPO_URL}" -o ! "${PING_ARTIFACT_REPO_URL#s3}" == "${PING_ARTIFACT_REPO_URL}"; then
-          installAwsCliTools
-        fi
-
         DOWNLOAD_DIR="${STAGING_DIR}/pd.profile/server-sdk-extensions"
 
         # Create extensions folder
@@ -95,9 +90,18 @@ if test -f "${STAGING_DIR}/artifacts/artifact-list.json"; then
 
                 echo "Download Artifact from ${ARTIFACT_LOCATION}"
 
-                # Use aws command if ARTIFACT_LOCATION is in s3 format otherwise use curl
-                if ! test ${ARTIFACT_LOCATION#s3} == "${ARTIFACT_LOCATION}"; then
-                  aws s3 cp "${ARTIFACT_LOCATION}/${ARTIFACT_RUNTIME_ZIP}" ${DOWNLOAD_DIR}
+                # Use skbn command if ARTIFACT_LOCATION is cloud storage otherwise use curl
+                if test ${ARTIFACT_LOCATION#s3} != "${ARTIFACT_LOCATION}"; then
+
+                  # Set required environment variables for skbn
+                  initializeSkbnConfiguration "${ARTIFACT_LOCATION}"
+                  
+                  echo "Copying: '${ARTIFACT_RUNTIME_ZIP}' to '${SKBN_K8S_PREFIX}'"
+
+                  if ! skbnCopy "${SKBN_CLOUD_PREFIX}/${ARTIFACT_RUNTIME_ZIP}" "${SKBN_K8S_PREFIX}}${DOWNLOAD_DIR}"; then
+                    exit 1
+                  fi
+
                 else
                   # For downloading over https we need to specify the exact file name,
                   # This will only work for standard extensions with a prefix of pingidentity.com
