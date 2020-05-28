1ff999daec42dd6f6467877312a489bd7da48b27
savithaganapathi <savithaganapathi@pingidentity.com>
2020-05-28 17:50:40 -0500

    [skip pipeline] attempts to fix ds-cfg-hostname

--- profiles/aws/pingdirectory/hooks/183-run-setup.sh
+++ profiles/aws/pingdirectory/hooks/183-run-setup.sh
@@ -15,7 +15,7 @@ HEAP_SIZE_INT=$(echo "${MAX_HEAP_SIZE}" | grep 'g$' | cut -d'g' -f1)
 
 if test ! -z "${HEAP_SIZE_INT}" && test "${HEAP_SIZE_INT}" -ge 4; then
   NEW_HEAP_SIZE=$((HEAP_SIZE_INT - 2))g
-  echo "Changing manage-profile heap size to ${NEW_HEAP_SIZE}"
+  echo "setup: changing manage-profile heap size to ${NEW_HEAP_SIZE}"
   export UNBOUNDID_JAVA_ARGS="-client -Xmx${NEW_HEAP_SIZE} -Xms${NEW_HEAP_SIZE}"
 fi
 
@@ -28,8 +28,14 @@ else
   export PD_LDAP_PORT="${LDAPS_PORT}"
 fi
 
-echo "Using public host:port of ${PD_PUBLIC_HOSTNAME}:${PD_LDAP_PORT}"
-echo "hostname=${PD_PUBLIC_HOSTNAME}" > "${SERVER_ROOT_DIR}"/config/server.host
+echo "setup: using public host:port of ${PD_PUBLIC_HOSTNAME}:${PD_LDAP_PORT}"
+SERVER_HOST_FILE="${SERVER_ROOT_DIR}"/config/server.host
+
+echo "setup: adding PD_PUBLIC_HOSTNAME ${PD_PUBLIC_HOSTNAME} to ${SERVER_HOST_FILE} before setup"
+echo "hostname=${PD_PUBLIC_HOSTNAME}" > "${SERVER_HOST_FILE}"
+
+echo "setup: contents of ${SERVER_HOST_FILE} before setup"
+cat "${SERVER_HOST_FILE}"
 
 "${SERVER_ROOT_DIR}"/bin/manage-profile setup \
     --profile "${PD_PROFILE}" \
@@ -39,16 +45,29 @@ echo "hostname=${PD_PUBLIC_HOSTNAME}" > "${SERVER_ROOT_DIR}"/config/server.host
     --rejectFile /tmp/rejects.ldif
 
 MANAGE_PROFILE_STATUS=${?}
-echo "manage-profile setup status: ${MANAGE_PROFILE_STATUS}"
+echo "setup: manage-profile setup status: ${MANAGE_PROFILE_STATUS}"
 
-# Re-add the hostname, manage-profile setup loses it.
-echo "hostname=${PD_PUBLIC_HOSTNAME}" > "${SERVER_ROOT_DIR}"/config/server.host
+echo "setup: contents of ${SERVER_HOST_FILE} after setup"
+cat "${SERVER_HOST_FILE}"
+
+# Re-add the hostname in case manage-profile setup loses it.
+echo "setup: adding PD_PUBLIC_HOSTNAME ${PD_PUBLIC_HOSTNAME} to ${SERVER_HOST_FILE} after setup"
+echo "hostname=${PD_PUBLIC_HOSTNAME}" > "${SERVER_HOST_FILE}"
+
+echo "setup: contents of ${SERVER_HOST_FILE} after restore post-setup"
+cat "${SERVER_HOST_FILE}"
 
 export UNBOUNDID_JAVA_ARGS="${ORIG_UNBOUNDID_JAVA_ARGS}"
 
 if test "${MANAGE_PROFILE_STATUS}" -ne 0; then
-  echo "Contents of manage-profile.log file:"
+  echo "setup: contents of manage-profile.log file:"
   cat "${SERVER_ROOT_DIR}/logs/tools/manage-profile.log"
+
   test -f /tmp/rejects.ldif && cat /tmp/rejects.ldif
+
+  echo "setup: server install failed"
   exit 183
 fi
+
+echo "setup: server install complete"
+exit 0
