f07d0d00b5b0a26ed0cf4325c02d3c25e3cd507b
Suraj Thapa <surajthapa@pingidentity.com>
2020-06-11 01:46:26 +0000

    PDO-987 PA SKBN

--- profiles/aws/pingaccess/hooks/utils.lib.sh
+++ profiles/aws/pingaccess/hooks/utils.lib.sh
@@ -136,57 +136,6 @@ function pingaccess_admin_wait() {
 }
 
 ########################################################################################################################
-# Function to install AWS command line tools
-#
-########################################################################################################################
-function installAwsCliTools() {
-  if test -z "$(which aws)"; then
-    #   
-    #  Install AWS platform specific tools
-    #
-    echo "Installing AWS CLI tools for S3 support"
-    #
-    # TODO: apk needs to move to the Docker file as the package manager is plaform specific
-    #
-    apk --update add python3
-    pip3 install --no-cache-dir --upgrade pip
-    pip3 install --no-cache-dir --upgrade awscli
-  fi
-}
-
-########################################################################################################################
-# Function calls installAwsCliTools() and sets required environment variables for AWS S3 bucket
-#
-########################################################################################################################
-function initializeS3Configuration() {
-  unset BUCKET_URL_NO_PROTOCOL
-  unset BUCKET_NAME
-  unset DIRECTORY_NAME
-  unset TARGET_URL
-
-  # Allow overriding the backup URL with an arg
-  test ! -z "${1}" && BACKUP_URL="${1}"
-
-  # Install AWS CLI if the upload location is S3
-  if test "${BACKUP_URL#s3}" == "${BACKUP_URL}"; then
-    echo "Upload location is not S3"
-    exit 1
-  else
-    installAwsCliTools
-  fi
-
-  export BUCKET_URL_NO_PROTOCOL=${BACKUP_URL#s3://}
-  export BUCKET_NAME=$(echo "${BUCKET_URL_NO_PROTOCOL}" | cut -d/ -f1)
-  export DIRECTORY_NAME=$(echo "${PING_PRODUCT}" | tr '[:upper:]' '[:lower:]')
-
-  if test "${BACKUP_URL}" == */"${DIRECTORY_NAME}"; then
-    export TARGET_URL="${BACKUP_URL}"
-  else
-    export TARGET_URL="${BACKUP_URL}/${DIRECTORY_NAME}"
-  fi
-}
-
-########################################################################################################################
 # Function to change password.
 #
 ########################################################################################################################
@@ -282,4 +231,65 @@ function comparePasswordDiskWithVariable() {
   fi
   "${VERBOSE}" && set -x
   return 0
+}
+
+#########################################################################################################################
+# Function sets required environment variables for skbn
+#
+########################################################################################################################
+function initializeSkbnConfiguration() {
+  unset SKBN_CLOUD_PREFIX
+  unset SKBN_K8S_PREFIX
+
+  # Allow overriding the backup URL with an arg
+  test ! -z "${1}" && BACKUP_URL="${1}"
+
+  # Check if endpoint is AWS cloud stroage service (S3 bucket)
+  case "$BACKUP_URL" in "s3://"*)
+    
+    # Set AWS specific variable for skbn
+    export AWS_REGION=${REGION}
+    
+    DIRECTORY_NAME=$(echo "${PING_PRODUCT}" | tr '[:upper:]' '[:lower:]')
+
+    if test "${BACKUP_URL}" != */"${DIRECTORY_NAME}"; then
+      BACKUP_URL="${BACKUP_URL}/${DIRECTORY_NAME}"
+    fi
+
+  esac
+
+  echo "Getting cluster metadata"
+
+  # Get prefix of HOSTNAME which match the pod name.  
+  POD="$(echo "${HOSTNAME}" | cut -d. -f1)"
+  
+  METADATA=$(kubectl get "$(kubectl get pod -o name | grep "${POD}")" \
+    -o=jsonpath='{.metadata.namespace},{.metadata.name},{.metadata.labels.role}')
+    
+  METADATA_NS=$(echo "$METADATA"| cut -d',' -f1)
+  METADATA_PN=$(echo "$METADATA"| cut -d',' -f2)
+  METADATA_CN=$(echo "$METADATA"| cut -d',' -f3)
+  
+  # Remove suffix for runtime.
+  METADATA_CN="${METADATA_CN%-engine}"
+
+  export SKBN_CLOUD_PREFIX="${BACKUP_URL}"
+  export SKBN_K8S_PREFIX="k8s://${METADATA_NS}/${METADATA_PN}/${METADATA_CN}"
+}
+
+########################################################################################################################
+# Function to copy file(s) between cloud storage and k8s
+#
+########################################################################################################################
+function skbnCopy() {
+  PARALLEL="0"
+  SOURCE="${1}"
+  DESTINATION="${2}"
+
+  # Check if the number of files to be copied in parallel is defined (0 for full parallelism)
+  test ! -z "${3}" && PARALLEL="${3}"
+  
+  if ! skbn cp --src "$SOURCE" --dst "${DESTINATION}" --parallel "${PARALLEL}"; then
+    return 1
+  fi
 }
