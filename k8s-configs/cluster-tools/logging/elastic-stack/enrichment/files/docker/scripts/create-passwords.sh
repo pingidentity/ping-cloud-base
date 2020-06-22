6359d0e3c735e4ebe3680f2f86fd183f524a86d6
Anton Klyba <antonklyba@pingidentity.com>
2020-06-22 13:24:05 +0300

    [skip pipeline] move custom images to GCR, upgrade kibana to 7.6.0

--- k8s-configs/cluster-tools/logging/elastic-stack/enrichment/files/docker/scripts/create-passwords.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-source /scripts/logger.sh
-
-# HERE SHOULD BE ELASTICSEARCH PASSWORDS CREATION
-
-logger "INFO" "$CONTAINER_NAME: Job done!"
--- k8s-configs/cluster-tools/logging/elastic-stack/enrichment/files/docker/scripts/get-kibana-config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-source /scripts/logger.sh
-
-# HERE SHOULD BE FUNCTION THAT OBTAINS KIBANA DASHBOARD TEMPLATE
-
-logger "INFO" "$CONTAINER_NAME: Job done!"
