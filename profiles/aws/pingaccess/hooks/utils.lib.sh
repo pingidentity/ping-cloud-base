87822462cde64af69aca2a31f9e63dcc787cc4d2
savithaganapathi <savithaganapathi@pingidentity.com>
2020-07-13 22:37:46 -0500

    [skip pipeline]
    PDO-633: use the short host name for the ENGINE_NAME if primary cluster

--- profiles/aws/pingaccess/hooks/utils.lib.sh
+++ profiles/aws/pingaccess/hooks/utils.lib.sh
@@ -327,10 +327,13 @@ function export_config_settings() {
     MULTI_CLUSTER=true
     export CLUSTER_CONFIG_HOST="${PA_ADMIN_PUBLIC_HOSTNAME}"
     export CLUSTER_CONFIG_PORT=443
-    is_secondary_cluster &&
-      export ADMIN_HOST_PORT="${PA_ADMIN_PUBLIC_HOSTNAME}:443" ||
+    if is_secondary_cluster; then
+      export ADMIN_HOST_PORT="${PA_ADMIN_PUBLIC_HOSTNAME}:443"
+      export ENGINE_NAME="${PA_ENGINE_PUBLIC_HOSTNAME}:300${ORDINAL}"
+    else
       export ADMIN_HOST_PORT="${K8S_SERVICE_NAME_PINGACCESS_ADMIN}:9000"
-    export ENGINE_NAME="${PA_ENGINE_PUBLIC_HOSTNAME}:300${ORDINAL}"
+      export ENGINE_NAME=${SHORT_HOST_NAME}
+    fi
   else
     MULTI_CLUSTER=false
     export CLUSTER_CONFIG_HOST="${K8S_SERVICE_NAME_PINGACCESS_ADMIN}"
