2fe23831956f96d89ca1fdf76aa3a86004e6a3ed
Calvin Carter <calvincarter@pingidentity.com>
2020-08-31 19:55:43 +0000

    PDO-1261: Diff Bug Fix

--- ci-scripts/test/unit/pingdirectory/00-offline-enable.sh
+++ ci-scripts/test/unit/pingdirectory/00-offline-enable.sh
@@ -20,6 +20,8 @@ setUp() {
   touch "${HOOKS_DIR}"/utils.lib.sh
 
   export regions_file=$(mktemp)
+  export descriptor_json=$(mktemp)
+  export config_json=$(mktemp)
 
   SCRIPT_HOME=$(cd $(dirname ${0}); pwd)
   templates_dir_path="${SCRIPT_HOME}"/templates/offline-enable
@@ -32,12 +34,12 @@ oneTimeTearDown() {
 
 testParamsOfflineEnableScript() {
   # Set config_json variable, which is used by utils.offline-enable.sh
-  export config_json="${templates_dir_path}"/01-valid-offline-enable-config.json
+  tr -d '[:space:]' < "${templates_dir_path}"/01-valid-offline-enable-config.json > "${config_json}"
   verifyParams > /dev/null
   actual_exit_code=$?
   assertEquals "All required params injected into offline-enable expected to pass" 0 ${actual_exit_code}
 
-  export config_json="${templates_dir_path}"/03-missing-params-local-region-offline-enable-config.json
+  tr -d '[:space:]' < "${templates_dir_path}"/03-missing-params-local-region-offline-enable-config.json > "${config_json}"
   verifyParams > /dev/null
   actual_exit_code=$?
   assertEquals "The missing required param, local_region, is expected to fail when injected into script" 1 ${actual_exit_code}
@@ -46,17 +48,17 @@ testParamsOfflineEnableScript() {
 testDescriptorJsonSyntax() {
   # Set descriptor_json variable, which is used by utils.offline-enable.sh
 
-  export descriptor_json="${templates_dir_path}"/00-valid-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}"/00-valid-descriptor.json > "${descriptor_json}"
   validateDescriptorJsonSyntax
   actual_exit_code=$?
   assertEquals "Valid descriptor JSON expected to pass" 0 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/02-invalid-syntax-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}"/02-invalid-syntax-descriptor.json > "${descriptor_json}"
   validateDescriptorJsonSyntax
   actual_exit_code=$?
   assertEquals "Un-parsable descriptor JSON expected to fail" 1 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/10-empty-file.json
+  tr -d '[:space:]' < "${templates_dir_path}"/10-empty-file.json > "${descriptor_json}"
   validateDescriptorJsonSyntax
   actual_exit_code=$?
   assertEquals "Empty descriptor JSON expected to fail" 1 ${actual_exit_code}
@@ -65,37 +67,39 @@ testDescriptorJsonSyntax() {
 testDescriptorJsonSchema() {
   # Set export descriptor_json variable, which is used by utils.offline-enable.sh
 
-  export descriptor_json="${templates_dir_path}"/00-valid-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}/00-valid-descriptor.json" > "${descriptor_json}"
   verifyDescriptorJsonSchema > /dev/null
   actual_exit_code=$?
   assertEquals "Valid descriptor JSON expected to pass" 0 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/09-spaces-within-region-name.json
+  # If descriptor.json has spaces within keys. User will be notified with error.
+  # For this test we will purposely not trim the descriptor file and just use copy to simulate we are validating spaces within keys.
+  cp "${templates_dir_path}/09-spaces-within-region-name.json" "${descriptor_json}"
   verifyDescriptorJsonSchema > /dev/null
   actual_exit_code=$?
   assertEquals "Spaces in region name in descriptor JSON expected to fail" 1 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/04-empty-json-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}/04-empty-json-descriptor.json" > "${descriptor_json}"
   verifyDescriptorJsonSchema > /dev/null
   actual_exit_code=$?
   assertEquals "No regions in descriptor JSON expected to fail" 1 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/05-duplicate-region-name-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}/05-duplicate-region-name-descriptor.json" > "${descriptor_json}"
   verifyDescriptorJsonSchema > /dev/null
   actual_exit_code=$?
   assertEquals "Duplicate region names in descriptor JSON expected to fail" 1 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/06-missing-hostname-json-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}/06-missing-hostname-json-descriptor.json" > "${descriptor_json}"
   verifyDescriptorJsonSchema > /dev/null
   actual_exit_code=$?
   assertEquals "Missing hostname in descriptor JSON expected to fail" 1 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/07-invalid-count-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}/07-invalid-count-descriptor.json" > "${descriptor_json}"
   verifyDescriptorJsonSchema > /dev/null
   actual_exit_code=$?
   assertEquals "Invalid replica count in descriptor JSON expected to fail" 1 ${actual_exit_code}
 
-  export descriptor_json="${templates_dir_path}"/08-missing-count-json-descriptor.json
+  tr -d '[:space:]' < "${templates_dir_path}/08-missing-count-json-descriptor.json" > "${descriptor_json}"
   verifyDescriptorJsonSchema > /dev/null
   actual_exit_code=$?
   assertEquals "Missing replica count in descriptor JSON expected to fail" 1 ${actual_exit_code}
