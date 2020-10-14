e47edf2d1302c5699e49e1d79008ce08d7c4a88c
savithaganapathi <savithaganapathi@pingidentity.com>
2020-10-14 12:08:16 -0500

    PDO-1518: hook script to initialize PD replication data from seed server

--- profiles/aws/pingdirectory/hooks/81-initialize-replication-data.sh
+++ profiles/aws/pingdirectory/hooks/81-initialize-replication-data.sh
@@ -3,12 +3,14 @@
 . "${HOOKS_DIR}/pingcommon.lib.sh"
 . "${HOOKS_DIR}/utils.lib.sh"
 
+test -f "${HOOKS_DIR}/pingdirectory.lib.sh" && . "${HOOKS_DIR}/pingdirectory.lib.sh"
+
 if test "${ORDINAL}" -ne 0 || ! is_primary_cluster; then
   beluga_error 'replication cannot be initialized - this script must be run on the first server in the primary cluster'
   exit 1
 fi
 
-BASE_DNS=${BASE_DNS:-${DN_LIST}}
+BASE_DNS=${BASE_DNS:-${DNS_TO_ENABLE}}
 
 beluga_log "replication will be initialized for base DNs: ${BASE_DNS}"
 initStatus=0
