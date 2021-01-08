5b67c0b833b615a434642c248c3855b0f38ef972
Geoff Lancaster <geofflancaster@pingidentity.com>
2021-01-07 18:56:48 -0600

    no longer using common.sh

--- ci-scripts/build/configure_s3.sh
+++ ci-scripts/build/configure_s3.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
-SCRIPT_HOME=$(
-    cd $(dirname ${0})
-    pwd
-)
-. "${SCRIPT_HOME}"/../common.sh
+echo "Configuring AWS CLI"
+mkdir -p ~/.aws
 
-# Configure aws and kubectl, unless skipped
-configure_aws
+cat >~/.aws/config <<EOF
+  [default]
+  output = json
+
+  [profile ${AWS_PROFILE}]
+  output = json
+  region = ${AWS_DEFAULT_REGION}
+  source_profile = default
+  role_arn = ${AWS_ACCOUNT_ROLE_ARN}
+EOF
+
+cat >~/.aws/credentials <<EOF
+  [default]
+  aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+  aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+
+  [${AWS_PROFILE}]
+  role_arn = ${AWS_ACCOUNT_ROLE_ARN}
+EOF
