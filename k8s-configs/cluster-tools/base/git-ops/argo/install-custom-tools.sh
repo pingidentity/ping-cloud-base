7d209b12b39a716891b75239e478b72041396ca0
Savitha Ganapathi <savithaganapathi@pingidentity.com>
2021-01-11 22:13:26 +0000

    PDO-1564: Replacing FluxCD with ArgoCD (using SSH for git cloning)
    [skip pipeline]

---
+++ k8s-configs/cluster-tools/base/git-ops/argo/install-custom-tools.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -ex
+
+### git-remote-codecommit ###
+pip install --upgrade pip
+pip3 install git-remote-codecommit --no-warn-script-location
+
+cp /usr/local/bin/git-remote-codecommit /tools
+cp /usr/local/lib/python3.7/site-packages/git_remote_codecommit/__init__.py /tools
+
+# On the ArgoCD container, python3 is available under /usr/bin/python3
+sed -i 's|/usr/local/bin/python|/usr/bin/python3|' /tools/git-remote-codecommit
+chmod a+x /tools/git-remote-codecommit
+
+### envsubst and wget ###
+apt-get update
+apt-get -y install gettext-base wget
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+cp /usr/bin/envsubst /tools
+
+### kustomize v3.5.4 ###
+# Pins kustomize to v3.5.4 (later versions have performance problems)
+wget -qO /tools/kustomize_3_5_4 \
+    https://ping-artifacts.s3-us-west-2.amazonaws.com/pingcommon/kustomize/3.5.4/linux_amd64/kustomize
+chmod a+x /tools/kustomize_3_5_4
