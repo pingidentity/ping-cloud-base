bb68e7079d8892628a0d45709aeae793ca60ee60
savithaganapathi <savithaganapathi@pingidentity.com>
2021-02-05 16:27:36 -0600

    PDO-1397: update CSR script and corresponding changes to gen/push scripts
    Setting up an update branch to make it easier to update to v1.7.0

--- code-gen/update-cluster-state-wrapper.sh
+++ code-gen/update-cluster-state-wrapper.sh
@@ -30,62 +30,16 @@
 # operator on how to complete the migration and test the new cluster state.
 
 ### Global variables and utility functions ###
-PING_CLOUD_BASE='ping-cloud-base'
 UPDATE_SCRIPT_NAME='update-cluster-state.sh'
 CODE_GEN_DIR_NAME='code-gen'
-
-########################################################################################################################
-# Invokes pushd on the provided directory but suppresses stdout and stderr.
-#
-# Arguments
-#   ${1} -> The directory to push.
-########################################################################################################################
-pushd_quiet() {
-  # shellcheck disable=SC2164
-  pushd "$1" >/dev/null 2>&1
-}
-
-########################################################################################################################
-# Invokes popd but suppresses stdout and stderr.
-########################################################################################################################
-popd_quiet() {
-  # shellcheck disable=SC2164
-  popd >/dev/null 2>&1
-}
 
 ### SCRIPT START ###
 
 # Verify that required environment variable NEW_VERSION is set
-if test -z "${NEW_VERSION}"; then
-  echo '=====> NEW_VERSION environment variable must be set before invoking this script'
+if test -z "${NEW_PING_CLOUD_BASE_REPO}"; then
+  echo '=====> NEW_PING_CLOUD_BASE_REPO environment variable must be set before invoking this script'
   exit 1
 fi
 
-# Clone ping-cloud-base at the new version, if necessary.
-if ! test "${NEW_PING_CLOUD_BASE_REPO}"; then
-  PCB_CLONE_BASE_DIR="$(mktemp -d)"
-  PING_CLOUD_BASE_REPO_URL="${PING_CLOUD_BASE_REPO_URL:-$(git grep ^K8S_GIT_URL= | head -1 | cut -d= -f2)}"
-  PING_CLOUD_BASE_REPO_URL="${PING_CLOUD_BASE_REPO_URL:-https://github.com/pingidentity/${PING_CLOUD_BASE}}"
-
-  pushd_quiet "${PCB_CLONE_BASE_DIR}"
-  echo "=====> Cloning ${PING_CLOUD_BASE}@${NEW_VERSION} from ${PING_CLOUD_BASE_REPO_URL} to '${PCB_CLONE_BASE_DIR}'"
-  git clone --depth 1 --branch "${NEW_VERSION}" "${PING_CLOUD_BASE_REPO_URL}"
-  if test $? -ne 0; then
-    echo "=====> Unable to clone ${PING_CLOUD_BASE_REPO_URL}@${NEW_VERSION} from ${PING_CLOUD_BASE_REPO_URL}"
-    popd_quiet
-    exit 1
-  fi
-  popd_quiet
-
-  NEW_PING_CLOUD_BASE_REPO="${PCB_CLONE_BASE_DIR}/${PING_CLOUD_BASE}"
-fi
-
-UPDATE_SCRIPT_PATH="${NEW_PING_CLOUD_BASE_REPO}/${CODE_GEN_DIR_NAME}/${UPDATE_SCRIPT_NAME}"
-
-if test -f "${UPDATE_SCRIPT_PATH}"; then
-  "${UPDATE_SCRIPT_PATH}"
-  exit $?
-else
-  echo "=====> Upgrade script not supported to update the cluster state repo to version ${NEW_VERSION}"
-  exit 1
-fi
+NEW_VERSION=1.7.0 "${NEW_PING_CLOUD_BASE_REPO}/${CODE_GEN_DIR_NAME}/${UPDATE_SCRIPT_NAME}"
+exit $?
