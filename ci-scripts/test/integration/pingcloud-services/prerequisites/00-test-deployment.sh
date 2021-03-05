4c50e250bca34546fe11dac82d7e4722bdcf6cff
Matt Peterson <mpeterson@pingidentity.com>
2021-03-05 20:25:56 +0000

    PDO-1758: Initial impl of OAuth client services controller
    [skip pipeline]

---
+++ ci-scripts/test/integration/pingcloud-services/prerequisites/00-test-deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. "${PROJECT_DIR}"/ci-scripts/common.sh "${1}"
+
+if skipTest "${0}"; then
+  log "Skipping test ${0}"
+  exit 0
+fi
+
+testDeploymentAvailability() {
+
+  status=$(kubectl get deployment p14c-oauth-service -o json | jq -r '.status.conditions[0].type')
+  assertEquals 0 $?
+  assertEquals "The status of the p14c-oauth-service deployment should be Available but was: ${status}" 'Available' ${status}
+}
+
+
+# When arguments are passed to a script you must
+# consume all of them before shunit is invoked
+# or your script won't run.  For integration
+# tests, you need this line.
+shift $#
+
+# load shunit
+. ${SHUNIT_PATH}
