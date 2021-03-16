f76ee5e6377ee9efa9a85274131ce04c3409b161
Jay Eno <jayeno@pingidentity.com>
2021-03-15 19:47:24 -0600

    Fixing Test

--- ci-scripts/test/integration/pingfederate/11-data-store.sh
+++ ci-scripts/test/integration/pingfederate/11-data-store.sh
@@ -7,42 +7,40 @@ if skipTest "${0}"; then
   exit 0
 fi
 
-export PF_ADMIN_USERNAME="Administrator"
+export PF_ADMIN_USERNAME="administrator"
 export PF_ADMIN_PASSWORD="2FederateM0re"
 export LDAP_DS_ID="LDAP-FA8D375DFAC589A222E13AA059319ABF9823B552"
 
 function make_api_request() {
-  set +x
-  http_code=$(curl -k -o ${OUT_DIR}/api_response.txt -w "%{http_code}" \
-        --retry ${API_RETRY_LIMIT} \
-        --max-time ${API_TIMEOUT_WAIT} \
-        --retry-delay 1 \
-        --retry-connrefused \
-        -u ${PF_ADMIN_USER_USERNAME}:${PF_ADMIN_USER_PASSWORD} \
+  http_code=$(curl -k -o /dev/null -w "%{http_code}" \
+        -u ${PF_ADMIN_USERNAME}:${PF_ADMIN_PASSWORD} \
         -H "Content-Type: application/json" \
         -H 'X-Xsrf-Header: PingFederate' "$@")
   curl_result=$?
-  "${VERBOSE}" && set -x
 
   if test "${curl_result}" -ne 0; then
-    beluga_error "Admin API connection refused"
     return ${curl_result}
   fi
 
   if test "${http_code}" -ne 200; then
-    beluga_log "API call returned HTTP status code: ${http_code}"
     return 1
   fi
-
-  rm -f ${OUT_DIR}/api_response.txt
 
   return 0
 }
 
 testDataStoreExists() {
-  response=$(make_api_request -s -X GET "${PINGFEDERATE_ADMIN_API}/dataStores/${LDAP_DS_ID}") > /dev/null)
+  response=$(make_api_request -s -X GET "${PINGFEDERATE_ADMIN_API}/dataStores/${LDAP_DS_ID}" > /dev/null)
   assertEquals "Response value was ${response}" 0 $?
 }
 
+# When arguments are passed to a script you must
+# consume all of them before shunit is invoked
+# or your script won't run.  For integration
+# tests, you need this line.
+shift $#
+
+# load shunit
+. ${SHUNIT_PATH}
 
 
