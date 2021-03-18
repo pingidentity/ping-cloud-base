30031a4bde4f1709160948b7b198a9b9078442b6
Ray Sutton <rsutton@pingidentity.com>
2021-03-18 13:32:38 -0600

    Add python k8s client example2

--- ci-scripts/python/framework.py
+++ ci-scripts/python/framework.py
@@ -1,6 +1,17 @@
-import sys
 from kubernetesHelper import KubernetesHelper
 
+print('-' * 77)
+print("PYTHON PROGRAM: Demo Test Framework k8s helper - list namespaces")
+print('')
+print("source code:")
+print('\tfrom kubernetesHelper import KubernetesHelper')
+print('')
+print('\tk8s = KubernetesHelper()')
+print('\tx = k8s.getNamespaces()')
+print('')
+print('\tfor y in x.items:')
+print('\t    print(y.metadata.name)')
+print('-' * 77)
 
 k8s = KubernetesHelper()
 x = k8s.getNamespaces()
