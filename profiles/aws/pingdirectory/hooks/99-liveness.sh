0dd2e64332a7c27e41cbe9d5dc1bcdbc477b7c6f
calvincarter-ping <calvincarter@pingidentity.com>
2021-04-19 14:05:40 -0500

    [skip pipeline] add appintegrations (clients, accessgrants, authsessions) and platformconfig

--- profiles/aws/pingdirectory/hooks/99-liveness.sh
+++ profiles/aws/pingdirectory/hooks/99-liveness.sh
@@ -8,17 +8,32 @@ test -f "${CONTAINER_ENV}" && . "${CONTAINER_ENV}"
 
 # shellcheck disable=SC2086
 ldapsearch \
-    --dontWrap \
-    --terse \
-    --suppressPropertiesFileComment \
-    --noPropertiesFile \
-    --operationPurpose "Docker container liveness check" \
-    --port "${LDAPS_PORT}" \
-    --useSSL \
-    --trustAll \
-    --baseDN "" \
-    --searchScope base "(&)" 1.1 \
-    2>/dev/null || exit 1
+  --dontWrap \
+  --terse \
+  --suppressPropertiesFileComment \
+  --noPropertiesFile \
+  --operationPurpose "Docker container liveness check" \
+  --port "${LDAPS_PORT}" \
+  --useSSL \
+  --trustAll \
+  --baseDN "" \
+  --searchScope base "(&)" 1.1 \
+  2>/dev/null || exit 1
 
 beluga_log "Test LDAP appintegrations Connection"
-ldapsearch o=appintegrations && exit 0 || exit 1
+ldapsearch \
+  ou=accessgrants,o=appintegrations \
+  2>/dev/null || exit 1
+
+ldapsearch \
+  ou=clients,o=appintegrations \
+  2>/dev/null || exit 1
+
+ldapsearch \
+  ou=authsessions,o=appintegrations \
+  2>/dev/null || exit 1
+
+beluga_log "Test LDAP platformconfig Connection"
+ldapsearch \
+  o=platformconfig \
+  2>/dev/null || exit 1
