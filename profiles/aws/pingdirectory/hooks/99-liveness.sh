1beab9724805c3a0416200c6ff3a08e87f5d1867
calvincarter-ping <calvincarter@pingidentity.com>
2021-04-20 14:57:47 -0500

    [skip pipeline] refactor PD 99-liveness probe, testing ldap searches, test1

--- profiles/aws/pingdirectory/hooks/99-liveness.sh
+++ profiles/aws/pingdirectory/hooks/99-liveness.sh
@@ -21,27 +21,12 @@ ldapsearch \
   --searchScope base "(&)" 1.1 \
   2>/dev/null || exit 1
 
-beluga_log "Test LDAP ou=accessgrants,o=appintegrations connection"
-ldapsearch \
-  ou=accessgrants,o=appintegrations \
-  2>/dev/null || exit 1
-
 beluga_log "Test LDAP ou=clients,o=appintegrations connection"
 ldapsearch \
-  ou=clients,o=appintegrations \
-  2>/dev/null || exit 1
-
-beluga_log "Test LDAP ou=authsessions,o=appintegrations connection"
-ldapsearch \
-  ou=authsessions,o=appintegrations \
-  2>/dev/null || exit 1
-
-beluga_log "Test LDAP uid=pingfederate,ou=devopsaccount,o=platformconfig connection"
-ldapsearch \
-  uid=pingfederate,ou=devopsaccount,o=platformconfig \
+  --baseDN "ou=clients,o=appintegrations" \
   2>/dev/null || exit 1
 
 beluga_log "Test LDAP ou=admins,o=platformconfig connection"
 ldapsearch \
-  ou=admins,o=platformconfig \
+  --baseDN "ou=admins,o=platformconfig" \
   2>/dev/null || exit 1
