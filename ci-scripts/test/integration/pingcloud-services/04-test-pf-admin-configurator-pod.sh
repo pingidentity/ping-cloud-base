72f590e4ea2872998c66c9e512e5b21cb195b2e4
Anton Klyba <antonklyba@pingidentity.com>
2021-06-23 18:56:41 +0300

    adds tests for PF-admin-configurator

--- ci-scripts/test/integration/pingcloud-services/04-test-pf-admin-configurator-pod.sh
+++ ci-scripts/test/integration/pingcloud-services/04-test-pf-admin-configurator-pod.sh
@@ -13,6 +13,32 @@ testPingFederateAdminConfiguratorPodStatus() {
   assertEquals "The status phase of the pingfederate-admin-configurator pod should be Succeeded but was: ${status}" 'Succeeded' ${status}
 }
 
+testPingFederateCreatePingOneConnection() {
+    connections=$(curl -s -k -u ${PF_ADMIN_USERNAME}:${PF_ADMIN_PASSWORD} \
+        -H "Content-Type: application/json" \
+        -H 'X-Xsrf-Header: PingFederate' \
+        -X GET "${PINGFEDERATE_ADMIN_API}/pingOneConnections")
+    name=$(echo ${connections}| jq .items[0].name)
+    active=$(echo ${connections}| jq .items[0].active)
+    
+    assertEquals "PingOne connection name should be PING_ONE_to_PING_FED_DEMO_Gateway but was: ${name}" "PING_ONE_to_PING_FED_DEMO_Gateway" ${name}
+    assertEquals "PingOne connection active status should be true but was: ${active}" "true" ${active}
+}
+
+testPingFederateConfiguratorLogs() {
+    JOB_NAME=pingfederate-admin-configurator
+    POD=
+
+    POD=$(kubectl -n "${NAMESPACE}" get pod -l  job-name="${JOB_NAME}" -o json | jq .items[0].metadata.name|sed 's/"//g')
+    LOG=$(kubectl -n "${NAMESPACE}" logs "${POD}")
+    assertContains "${LOG}" "PING_FED-DEMO_Gateway"
+    assertContains "${LOG}" "Gateway credential created successfully, passing to PF"
+    assertContains "${LOG}" "Added Demo LDAP Data Store"
+    assertContains "${LOG}" "Created LDAP PCV"
+    assertContains "${LOG}" "Created Notification Publisher"
+    assertContains "${LOG}" "Created HTML form adapter"
+    assertContains "${LOG}" "Created PingID adapter"
+}
 
 # When arguments are passed to a script you must
 # consume all of them before shunit is invoked
