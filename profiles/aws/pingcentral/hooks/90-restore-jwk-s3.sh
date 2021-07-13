9ffa3795753c333efa96d0aebc1797b6dec4e41c
calvincarter-ping <calvincarter@pingidentity.com>
2021-07-13 10:02:35 -0500

    [skip pipeline] add restore directory to restore script

--- profiles/aws/pingcentral/hooks/90-restore-jwk-s3.sh
+++ profiles/aws/pingcentral/hooks/90-restore-jwk-s3.sh
@@ -4,19 +4,33 @@
 
 ${VERBOSE} && set -x
 
-# Set required environment variables for skbn
-initializeSkbnConfiguration
+cleanUp() {
+  # Cleanup upload dir
+  rm -rf "${SERVER_RESTORE_DIR}"
+}
 
 JWK_FILE_NAME="pingcentral.jwk"
 MASTER_KEY_PATH="${SERVER_ROOT_DIR}/conf/${JWK_FILE_NAME}"
 
-beluga_log "Copying: '${JWK_FILE_NAME}' to '${SKBN_K8S_PREFIX}${MASTER_KEY_PATH}'"
+# This is the backup directory on the server
+SERVER_RESTORE_DIR="${OUT_DIR}/restore"
+cleanUp
+mkdir -p "${SERVER_RESTORE_DIR}"
 
+# This guarantees that cleanUp will always run, even if this script exits due to an error
+trap "cleanUp" EXIT
 
-if ! skbnCopy "${SKBN_CLOUD_PREFIX}/${JWK_FILE_NAME}" "${SKBN_K8S_PREFIX}${MASTER_KEY_PATH}"; then
+# Set required environment variables for skbn
+initializeSkbnConfiguration
+
+beluga_log "Copying: '${JWK_FILE_NAME}' to '${SERVER_RESTORE_DIR}/${JWK_FILE_NAME}'"
+
+if ! skbnCopy "${SKBN_CLOUD_PREFIX}/${JWK_FILE_NAME}" "${SERVER_RESTORE_DIR}/${JWK_FILE_NAME}"; then
   beluga_log "Cannot locate s3 bucket ${SKBN_CLOUD_PREFIX}/${JWK_FILE_NAME}"
   exit 1
 fi
+
+cp "${SERVER_RESTORE_DIR}/${JWK_FILE_NAME}" "${MASTER_KEY_PATH}"
 
 beluga_log "Successfully restored ${JWK_FILE_NAME} file"
 
