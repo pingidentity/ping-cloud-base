e712488bfb033aa0828dc24d1ba711ccbb8b3a7a
calvincarter-ping <calvincarter@pingidentity.com>
2021-07-13 09:35:55 -0500

    [skip pipeline] add UPLOAD_DIR, JWK_FILE_NAME, JWK_FILE variables to upload and restore scripts

--- profiles/aws/pingcentral/hooks/90-restore-jwk-s3.sh
+++ profiles/aws/pingcentral/hooks/90-restore-jwk-s3.sh
@@ -18,31 +18,6 @@ if ! skbnCopy "${SKBN_CLOUD_PREFIX}/${JWK_FILE_NAME}" "${SKBN_K8S_PREFIX}${MASTE
   exit 1
 fi
 
-#!/usr/bin/env sh
+beluga_log "Successfully restored ${JWK_FILE_NAME} file"
 
-. "${HOOKS_DIR}/pingcommon.lib.sh"
-. "${HOOKS_DIR}/utils.lib.sh"
-
-${VERBOSE} && set -x
-
-JWK_FILE="${SERVER_ROOT_DIR}/conf/pingcentral.jwk"
-
-if ! test -f "${JWK_FILE}"; then
-  beluga_log "Skipping JWK_FILE needs to be available"
-  exit 0
-fi
-
-beluga_log "Uploading to location ${CHUB_BACKUP_URL}"
-
-# Set required environment variables for skbn
-initializeSkbnConfiguration
-
-
-beluga_log "Copying files to '${SKBN_CLOUD_PREFIX}'"
-
-if ! skbnCopy "${SKBN_K8S_PREFIX}${JWK_FILE}" "${SKBN_CLOUD_PREFIX}/"; then
-  exit 1
-fi
-
-beluga_log "Successfully restored pingcentral.jwk file"
 exit 0
--- profiles/aws/pingcentral/hooks/90-upload-jwk-s3.sh
+++ profiles/aws/pingcentral/hooks/90-upload-jwk-s3.sh
@@ -5,11 +5,21 @@
 
 ${VERBOSE} && set -x
 
-JWK_FILE="${SERVER_ROOT_DIR}/conf/pingcentral.jwk"
+cleanUp() {
+  # Cleanup upload dir
+  rm -rf "${UPLOAD_DIR}"
+}
+
+# This guarantees that cleanUp will always run, even if this script exits due to an error
+trap "cleanUp" EXIT
+
+JWK_FILE_NAME="pingcentral.jwk"
+JWK_FILE="${SERVER_ROOT_DIR}/conf/${JWK_FILE_NAME}"
+UPLOAD_DIR="$(mktemp -d)"
 
 if ! test -f "${JWK_FILE}"; then
-  beluga_log "Skipping JWK_FILE needs to be available"
-  exit 0
+  beluga_log "Skipping ${JWK_FILE} needs to be available"
+  exit 1
 fi
 
 beluga_log "Uploading to location ${CHUB_BACKUP_URL}"
@@ -17,13 +27,18 @@ beluga_log "Uploading to location ${CHUB_BACKUP_URL}"
 # Set required environment variables for skbn
 initializeSkbnConfiguration
 
+cp "${JWK_FILE}" "${UPLOAD_DIR}/${JWK_FILE_NAME}"
 
-beluga_log "Copying files to '${SKBN_CLOUD_PREFIX}'"
+beluga_log "Copying files in '${UPLOAD_DIR}' to '${SKBN_CLOUD_PREFIX}'"
 
-if ! skbnCopy "${SKBN_K8S_PREFIX}${JWK_FILE}" "${SKBN_CLOUD_PREFIX}/"; then
+if ! skbnCopy "${UPLOAD_DIR}" "${SKBN_CLOUD_PREFIX}"; then
+  beluga_log "Failed to upload files in ${UPLOAD_DIR}"
   exit 1
 fi
 
-beluga_log "Successfully uploaded pingcentral.jwk file"
+beluga_log "Successfully uploaded ${JWK_FILE_NAME} file"
+
+# STDOUT all the files in one line for integration test
+ls "${UPLOAD_DIR}" | xargs
 
 exit 0
