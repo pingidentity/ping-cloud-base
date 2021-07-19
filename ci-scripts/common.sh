15d690a3ee7ca28293d97759e44de0dfcf09b2c2
calvincarter-ping <calvincarter@pingidentity.com>
2021-07-19 10:55:55 -0500

    [skip pipeline] add pingcentral integration test

--- ci-scripts/common.sh
+++ ci-scripts/common.sh
@@ -80,6 +80,7 @@ export CLUSTER_NAME_LC=$(echo "${CLUSTER_NAME}" | tr '[:upper:]' '[:lower:]')
 export LOG_GROUP_NAME="/aws/containerinsights/${CLUSTER_NAME}/application"
 
 FQDN=${ENVIRONMENT}.${TENANT_DOMAIN}
+CHUB_FQDN=${TENANT_DOMAIN}
 
 # Monitoring
 LOGS_CONSOLE=https://logs${FQDN}/app/kibana
@@ -128,6 +129,9 @@ PINGDELEGATOR_CONSOLE=https://pingdelegator${FQDN}/delegator
 
 # Pingcloud-metadata service:
 PINGCLOUD_METADATA_API=https://metadata${FQDN}
+
+# PingCentral service
+PINGCENTRAL_CONSOLE=https://pingcentral${CHUB_FQDN}
 
 # Source some utility methods.
 . ${PROJECT_DIR}/utils.sh
