e07747c98c5eef619e3ae61ba18ffd1cd329644f
savithaganapathi <savithaganapathi@pingidentity.com>
2021-08-06 19:49:02 -0500

    PDO-2564: fixing PC ingress to be environment specific
    This has been causing issues on concurrent branch pipelines
    
    Fixing PINGCENTRAL_CONSOLE env variable

--- ci-scripts/common.sh
+++ ci-scripts/common.sh
@@ -87,7 +87,6 @@ export CLUSTER_NAME_LC=$(echo "${CLUSTER_NAME}" | tr '[:upper:]' '[:lower:]')
 export LOG_GROUP_NAME="/aws/containerinsights/${CLUSTER_NAME}/application"
 
 FQDN=${ENVIRONMENT}.${TENANT_DOMAIN}
-CHUB_FQDN=${TENANT_DOMAIN}
 
 # Monitoring
 LOGS_CONSOLE=https://logs${FQDN}/app/kibana
@@ -144,7 +143,7 @@ MYSQL_PASSWORD_SSM=/pcpt/ping-central/rds/password
 PINGCLOUD_METADATA_API=https://metadata${FQDN}
 
 # PingCentral service
-PINGCENTRAL_CONSOLE=https://pingcentral.${CHUB_FQDN}
+PINGCENTRAL_CONSOLE=https://pingcentral.${FQDN}
 
 # Source some utility methods.
 . ${PROJECT_DIR}/utils.sh
