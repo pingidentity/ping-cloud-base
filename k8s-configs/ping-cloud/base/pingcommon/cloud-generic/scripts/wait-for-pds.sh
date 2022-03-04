17c82bd16df8411dc3e92ef1c83ef07754286571
Calvin Carter <calvincarter@pingidentity.com>
2022-03-04 22:46:51 +0000

    PDO-3577, PDO-3564: Disable external server configuration. Use flag to enable for QA

--- k8s-configs/ping-cloud/base/pingcommon/cloud-generic/scripts/wait-for-pds.sh
+++ k8s-configs/ping-cloud/base/pingcommon/cloud-generic/scripts/wait-for-pds.sh
@@ -7,12 +7,12 @@ SLEEP_SECONDS=${INITIAL_DELAY_SECONDS:-0}
 beluga_log "Initial delay: ${SLEEP_SECONDS}"
 sleep "${SLEEP_SECONDS}"
 
-SYNC_SERVERS="${P1AS_PD_POD_NAME}.${P1AS_PD_CLUSTER_PRIVATE_HOSTNAME}:${P1AS_PD_LDAPS_PORT} \
- ${EXT_PD_HOST}:${EXT_PD_LDAPS_PORT}"
+SYNC_SERVERS="${P1AS_PD_POD_NAME}.${P1AS_PD_CLUSTER_PRIVATE_HOSTNAME}:${P1AS_PD_LDAPS_PORT}"
 
-# Append external PingDirectory HTTPS host if user want changelog enabled and max-age set for them.
-if [ "${SET_EXT_PD_CHANGELOG_MAX_AGE_FOR_ME}" = "true" ]; then
-  SYNC_SERVERS="${SYNC_SERVERS} ${EXT_PD_HOST}:${EXT_PD_HTTPS_PORT}"
+# Append external PingDirectory host ldap and https if IS_P1AS_TEST_MODE is true.
+if [ "${IS_P1AS_TEST_MODE}" = "true" ]; then
+  SYNC_SERVERS="${EXT_PD_HOST}:${EXT_PD_LDAPS_PORT} \
+  ${SYNC_SERVERS} ${EXT_PD_HOST}:${EXT_PD_HTTPS_PORT}"
 fi
 
 beluga_log "Checking sync servers: ${SYNC_SERVERS}"
