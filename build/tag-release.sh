2ece45e82f62895e09bc064c00600fec8af2d91b
vathsalyakidambi <vathsalyakidambi@pingidentity.com>
2022-04-25 08:56:34 -0500

    [skip pipeline] - creating new  v1.99-release-branch

--- build/tag-release.sh
+++ build/tag-release.sh
@@ -123,9 +123,9 @@ grep_var() {
 
 grep_yaml() {
 
-  image=${1}
-  SOURCE_VALUE=${2}
-  TARGET_VALUE=${3}
+  image=${0}
+  SOURCE_VALUE=${1}
+  TARGET_VALUE=${2}
 
   echo "Changing ${SOURCE_VALUE} -> ${TARGET_VALUE} in expected files"
   cd /Users/vathsalyakidambi/Desktop/repos/ping-cloud-base/k8s-configs
