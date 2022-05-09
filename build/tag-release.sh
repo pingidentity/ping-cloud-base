1f5014b2c9fb00b6636e21e6b5d1ee19c704525b
vathsalyakidambi <vathsalyakidambi@pingidentity.com>
2022-05-09 02:00:55 -0500

    [skip pipeline]

--- build/tag-release.sh
+++ build/tag-release.sh
@@ -6,9 +6,9 @@
 usage() {
   echo "Usage: ${0} SOURCE_REF TARGET_REF [REF_TYPE]"
   echo "  where"
-  echo "    SOURCE_REF => source ref from where to create the target ref, e.g. master"
-  echo "    TARGET_REF => the target ref, e.g. v1.0.0"
-  echo "    REF_TYPE => the target ref type - tag or branch, default is tag"
+  echo "    SOURCE_REF => source ref from where to create the target ref, e.g. v1.14-release-branch"
+  echo "    TARGET_REF => the target ref, e.g. v1.15-release-branch, v1.15.0.0_RC1"
+  echo "    REF_TYPE => the target ref type - tag or branch"
 }
 
 ########################################################################################################################
@@ -19,7 +19,7 @@ usage() {
 # Arguments:
 #   ${1} -> The source ref
 #   ${2} -> The target ref
-#   ${3} -> The ref type- tag
+#   ${3} -> The ref type- tag or branch
 ########################################################################################################################
 replaceAndCommit() {
   SOURCE=${1}
@@ -28,35 +28,32 @@ replaceAndCommit() {
 
   echo "Changing ${SOURCE} -> ${TARGET} in expected files"
 
-  git grep -l "^SERVER_PROFILE_BRANCH=${SOURCE}" | xargs sed -i.bak "s/^\(SERVER_PROFILE_BRANCH=\)${SOURCE}$/\1${TARGET}/g"
-
   #update base env vars
 
-  grep_var "PINGACCESS_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "PINGACCESS_WAS_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "PINGFEDERATE_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "PINGDIRECTORY_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "PINGDELEGATOR_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "PINGCENTRAL_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "PINGDATASYNC_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "METADATA_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "P14C_BOOTSTRAP_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "P14C_INTEGRATION_IMAGE_TAG" "${SOURCE}" "${TARGET}"
-  grep_var "ANSIBLE_BELUGA_IMAGE_TAG" "${SOURCE}" "${TARGET}"
+  grep_var "PINGACCESS_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "PINGACCESS_WAS_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "PINGFEDERATE_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "PINGDIRECTORY_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "PINGDELEGATOR_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "PINGCENTRAL_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "PINGDATASYNC_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "METADATA_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "P14C_BOOTSTRAP_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "P14C_INTEGRATION_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  grep_var "ANSIBLE_BELUGA_IMAGE_TAG" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
 
   #update k8s yaml files
 
-  grep_yaml "pingaccess" "pingcloud-apps" "${SOURCE}" "${TARGET}"
-  grep_yaml "pingaccess-was" "pingcloud-apps" "${SOURCE}" "${TARGET}"
-  grep_yaml "pingfederate" "pingcloud-apps" "${SOURCE}" "${TARGET}"
-  grep_yaml "pingdirectory" "pingcloud-apps" "${SOURCE}" "${TARGET}"
-  grep_yaml "pingdelegator" "pingcloud-apps" "${SOURCE}" "${TARGET}"
-  grep_yaml "pingcentral" "pingcloud-apps" "${SOURCE}" "${TARGET}"
-  grep_yaml "pingdatasync" "pingcloud-apps" "${SOURCE}" "${TARGET}"
-  grep_yaml "metadata" "pingcloud-services" "${SOURCE}" "${TARGET}"
-  grep_yaml "p14c-bootstrap" "pingcloud-services" "${SOURCE}" "${TARGET}"
-  grep_yaml "p14c-integration" "pingcloud-services" "${SOURCE}" "${TARGET}"
-  grep_yaml "ansible-beluga" "pingcloud-solutions" "${SOURCE}" "${TARGET}"
+  grep_yaml "pingaccess" "pingcloud-apps" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "pingaccess-was" "pingcloud-apps" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "pingfederate" "pingcloud-apps" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "pingdirectory" "pingcloud-apps" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "pingdelegator" "pingcloud-apps" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "pingcentral" "pingcloud-apps" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "pingdatasync" "pingcloud-apps" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "p14c-bootstrap" "pingcloud-services" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "p14c-integration" "pingcloud-services" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
+  # grep_yaml "ansible-beluga" "pingcloud-solutions" "${SOURCE}" "${TARGET}" "${REF_TYPE}"
 
   echo "Committing changes for new ${REF_TYPE} ${TARGET}"
   git add .
@@ -68,15 +65,11 @@ grep_var() {
   local VAR=${1}
   local SOURCE_VALUE=${2}
   local TARGET_VALUE=${3}
+  local REF_VALUE=${4}
 
-  verify_var=$(git grep -l "^${VAR}=${SOURCE_VALUE}" | wc -l)
-  if test ${verify_var} = 0; then
-    usage
-    exit 1
-  else
-    echo "Changing ${SOURCE_VALUE} -> ${TARGET_VALUE} in expected files"
-    git grep -l "^${VAR}=${SOURCE_VALUE}" | xargs sed -i.bak "s/^\(${VAR}=\)${SOURCE_VALUE}$/\1${TARGET_VALUE}/g"
-  fi
+  echo "Changing ${SOURCE_VALUE} -> ${TARGET_VALUE} in expected files"
+
+  git grep -l "^${VAR}=${SOURCE_VALUE}" | xargs sed -i.bak "s/^\(${VAR}=\)${SOURCE_VALUE}$/\1${TARGET_VALUE}/g"
 
 }
 
@@ -86,40 +79,30 @@ grep_yaml() {
   local ECR_REPO=${2}
   local SOURCE_VALUE=${3}
   local TARGET_VALUE=${4}
+  local REF_VALUE=${5}
 
-  local image="image: public.ecr.aws/r2h3l6e4/${2}/${1}"
+  local dev_ecr_path="image: public.ecr.aws/r2h3l6e4/${2}/${1}/dev"
+  local prod_ecr_path="image: public.ecr.aws/r2h3l6e4/${2}/${1}"
+  local SOURCE_IMAGE="${dev_ecr_path}:${SOURCE_VALUE}"
+
+  if test "${REF_VALUE}" = 'branch'; then
+    local TARGET_IMAGE="${dev_ecr_path}:${TARGET_VALUE}"
+
+  elif test "${REF_VALUE}" = 'tag'; then
+    local TARGET_IMAGE="${prod_ecr_path}:${TARGET_VALUE}"
+
+  else
+    usage
+    exit 1
+  fi
 
   cd "${SANDBOX}"/ping-cloud-base/k8s-configs
 
-  verify_yaml=$(git grep -l "${image}:${SOURCE_VALUE}" | wc -l)
-  if [[ ${verify_yaml} == 0 ]]; then
-    usage
-    exit 1
-  else
-    echo "Changing ${image}:${SOURCE_VALUE} -> ${image}:${TARGET_VALUE} in expected files"
-    git grep -l "${image}:${SOURCE_VALUE}" | xargs sed -i.bak "s/${SOURCE_VALUE}/${TARGET_VALUE}/g"
-  fi
+  echo "Changing ${SOURCE_IMAGE} -> ${TARGET_IMAGE} in expected files"
+  git grep -l "${SOURCE_IMAGE}" | xargs sed -i.bak "s/${SOURCE_IMAGE}/${TARGET_IMAGE}/g"
 
   cd "${SANDBOX}"/ping-cloud-base/
 
-}
-
-verify_ref_name() {
-
-  local value=${1}
-
-  # TODO: Change to 'release-branch'
-  REGEX='^v[0-9]+.[0-9]+-new-image-process$'
-
-  # REGEX='^pdo-[0-9]+$'
-
-  if [[ $value =~ $REGEX ]]; then
-    echo "$value is a release branch"
-    export REF_NAME="release-branch"
-  else
-    echo "$value is  RC"
-    export REF_NAME="rc"
-  fi
 }
 
 SOURCE_REF=${1}
@@ -147,43 +130,51 @@ git checkout "${SOURCE_REF}"
 ###########################################################################################################################
 # Verifies the 'REF_TYPE' if its a 'tag' or 'branch'.
 #
-# (1) -->if the target value is to be replaced by a 'tag' , 'verify_ref_name' method helps validate the source value if its
-#  a "release-branch(v*.*-release-branch)" or a "tag (RC)".
+# (1) -->if the 'REF_TYPE' is a 'branch' then --> creates new branch and adds new changes on the new branch as follows -
 #
+# updates 'SERVER_PROFILE_BRANCH' variable with target  branch name (v*.*-release-branch)
+# updates 'base/env_vars' image tags with target branch name (v*.*-release-branch-latest)
+# updates yaml files docker images with target branch name (v*.*-release-branch-latest)
 #
-# This would be applicable in phases "Ready to Release" and also during "Hardening" wherein the env_vars and other values
-# need to be replaced from 'v*.*-release-branch-latest' to 'RC tag' [or] if there is a new 'RC tag' that needs to be updated.
-# 
+# eg: `build/tag-release.sh v1.14-release-branch v1.15-release-branch branch`
 #
+# (2) -->if the 'REF_TYPE' is a 'tag' then --> adds new changes as follows and then creates a new tag -
 #
-# (2) -->if the target value is to be replaced by a 'branch' , 'verify_ref_name' method simply helps validate the target
-# value if its a "release-branch(v*.*-release-branch)" or not and breaks if any other value is provided.
+# updates 'SERVER_PROFILE_BRANCH' variable with target tag name (v*.*.*.*_RC1)
+# updates 'base/env_vars' image tags with target tag name (v*.*.*.*_RC1)
+# updates yaml files docker images target tag name (v*.*.*.*_RC1)
 #
+# eg: `build/tag-release.sh v1.14-release-branch v1.14.0.0_RC1`
 #
 ###########################################################################################################################
-if test "${REF_TYPE}" = 'tag'; then
-  verify_ref_name "${SOURCE_REF}"
-  if test "${REF_NAME}" = 'release-branch'; then
-    replaceAndCommit "${SOURCE_REF}-latest" "${TARGET_REF}" "${REF_TYPE}"
-  elif test "${REF_NAME}" = 'rc'; then
-    replaceAndCommit "${SOURCE_REF}" "${TARGET_REF}" "${REF_TYPE}"
-  else
-    usage
-    exit 1
-  fi
+
+if test "${REF_TYPE}" = 'branch'; then
+
+  # Create and checkout to target branch
+  git checkout -b "${TARGET_REF}"
+
+  # Update 'SERVER_PROFILE_BRANCH' variable
+  echo "Changing ${SOURCE_REF} -> ${TARGET_REF} in SERVER_PROFILE_BRANCH variable"
+  git grep -l "^SERVER_PROFILE_BRANCH=${SOURCE_REF}" | xargs sed -i.bak "s/^\(SERVER_PROFILE_BRANCH=\)${SOURCE_REF}$/\1${TARGET_REF}/g"
+
+  # Update 'base/env_vars' image tags and yaml files
+  echo "Changing ${SOURCE_REF}-latest -> ${TARGET_REF}-latest in base/env_vars and yaml files"
+  replaceAndCommit "${SOURCE_REF}-latest" "${TARGET_REF}-latest" "${REF_TYPE}"
+
+elif test "${REF_TYPE}" = 'tag'; then
+
+  # Update 'SERVER_PROFILE_BRANCH' variable
+  echo "Changing ${SOURCE_REF} -> ${TARGET_REF} in SERVER_PROFILE_BRANCH variable"
+  git grep -l "^SERVER_PROFILE_BRANCH=${SOURCE_REF}" | xargs sed -i.bak "s/^\(SERVER_PROFILE_BRANCH=\)${SOURCE_REF}$/\1${TARGET_REF}/g"
+
+  # Update 'base/env_vars' image tags and yaml files
+  echo "Changing ${SOURCE_REF}-latest -> ${TARGET_REF} in base/env_vars and yaml files"
+  replaceAndCommit "${SOURCE_REF}-latest" "${TARGET_REF}" "${REF_TYPE}"
   git tag "${TARGET_REF}"
-  unset REF_NAME
 
 else
-  verify_ref_name "${TARGET_REF}"
-  if test "${REF_NAME}" = 'release-branch'; then
-    git checkout -b "${TARGET_REF}"
-    replaceAndCommit "${SOURCE_REF}" "${TARGET_REF}-latest"
-  else
-    usage
-    exit 1
-  fi
-  unset REF_NAME
+  usage
+  exit 1
 fi
 
 echo ---
@@ -194,8 +185,6 @@ echo ---
 
 # Confirm before pushing the tag to the server
 read -n 1 -srp 'Press any key to continue'
-git push origin "${TARGET_REF}"
+# git push origin "${TARGET_REF}"
 
 popd &>/dev/null
-
-#todo- update "usage" method
