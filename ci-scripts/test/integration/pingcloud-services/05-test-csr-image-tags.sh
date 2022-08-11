6ed367bf069f96ab51c2e9479d2cb73fc56b5e29
Oleksii Kalinin <oleksiikalinin@pingidentity.com>
2022-08-11 14:28:28 +0200

    Review updates

--- ci-scripts/test/integration/pingcloud-services/05-test-csr-image-tags.sh
+++ ci-scripts/test/integration/pingcloud-services/05-test-csr-image-tags.sh
@@ -117,15 +117,15 @@ testMetadataImageTag() {
   assertEquals "PingCloud Metadata CSR image tag doesn't match Beluga default image tag" 1 "${matched_count}"
 }
 
-testP14CBootstrapImageTag() {
+testBootstrapImageTag() {
   $(test "${BOOTSTRAP_IMAGE_TAG}")
   assertEquals "BOOTSTRAP_IMAGE_TAG missing from env_vars file" 0 $?
 
   unique_count=$(getUniqueTagCount "bootstrap")
-  assertEquals "P14C Bootstrap is using multiple image tag versions" 1 "${unique_count}"
+  assertEquals "Bootstrap is using multiple image tag versions" 1 "${unique_count}"
 
   matched_count=$(getMatchedTagCount "${BOOTSTRAP_IMAGE_TAG}" "bootstrap")
-  assertEquals "P14C Bootstrap CSR image tag doesn't match Beluga default image tag" 1 "${matched_count}"
+  assertEquals "Bootstrap CSR image tag doesn't match Beluga default image tag" 1 "${matched_count}"
 }
 
 testP14CIntegrationImageTag() {
