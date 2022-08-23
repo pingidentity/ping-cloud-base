eea398868ffe5bbde31c65282a1401fbdcd259f5
Calvin Carter <calvincarter@pingidentity.com>
2022-08-23 20:06:19 +0000

    Merge Review 1: v1.15-release-branch into v1.16-release-branch

---
+++ ci-scripts/test/python-utils/health_common.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+import requests
+
+from k8s_utils import K8sUtils
+
+
+@dataclass
+class Categories:
+    pod_status = "pod-status"
+    synthetic = "synthetic"
+    data = "data"
+    connectivity = "connectivity"
+    cluster_members = "cluster-members"
+
+
+class TestHealthBase(K8sUtils):
+    job_name = ""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.run_job(cls.job_name)
+
+    def get_test_results(self, suite: str, category: str) -> {}:
+        """
+        Request test results from the healthcheck service and get a dictionary of the test names and PASS/FAIL result
+        :param suite: Test suite
+        :param category: Category within the test suite
+        :return: Test results dictionary in the format {"test name": "PASS/FAIL", ...}
+        """
+        response = requests.get(self.endpoint, verify=False)
+        return response.json()["health"][suite]["tests"][category]
