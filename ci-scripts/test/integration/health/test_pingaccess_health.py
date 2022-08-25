692b185ed3132187ee3ad83e2b9b0dc00c269724
Colin Reininger <colinreininger@pingidentity.com>
2022-08-25 22:24:53 +0000

    PDO-4104 Add PingAccess and PingAccess-WAS health checks cronjobs

---
+++ ci-scripts/test/integration/health/test_pingaccess_health.py
@@ -0,0 +1,47 @@
+import requests
+
+from health_common import Categories, TestHealthBase
+
+
+class TestPingAccessHealth(TestHealthBase):
+    job_name = "healthcheck-pingaccess"
+    pingaccess = "pingaccess"
+
+    def test_pingaccess_health_cron_job_exists(self):
+        cron_jobs = self.batch_client.list_cron_job_for_all_namespaces()
+        cron_job_name = next(
+            (
+                cron_job.metadata.name
+                for cron_job in cron_jobs.items
+                if cron_job.metadata.name == self.job_name
+            ),
+            "",
+        )
+        self.assertEqual(
+            self.job_name,
+            cron_job_name,
+            f"Cron job '{self.job_name}' not found in cluster",
+        )
+
+    def test_health_check_has_pingaccess_results(self):
+        res = requests.get(self.endpoint, verify=False)
+        self.assertTrue(
+            self.pingaccess in res.json()["health"].keys(),
+            f"No {self.pingaccess} in health check results",
+        )
+
+    def test_health_check_has_registered_results(self):
+        test_results = self.get_test_results(self.pingaccess, Categories.pod_status)
+        res = [key for key in test_results if "registered" in key]
+        self.assertTrue(
+            len(res) > 0,
+            "No 'registered with PA Admin' checks found in health check results",
+        )
+
+    def test_health_check_has_responsive_results(self):
+        test_results = self.get_test_results(self.pingaccess, Categories.connectivity)
+        res = [key for key in test_results if "responds" in key]
+        self.assertTrue(
+            len(res) > 0,
+            "No 'responds to requests' checks found in health check results",
+        )
