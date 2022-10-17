5dcf136df5ee007a0ce0e585f03d96f3927cc372
Anton Klyba <antonklyba@pingidentity.com>
2022-10-17 08:20:43 +0000

    PDO-4326: Implement must-have monitoring/alerting of PGO

--- utils.sh
+++ utils.sh
@@ -297,6 +297,12 @@ build_kustomizations_in_dir() {
   for KUSTOMIZATION_FILE in ${KUSTOMIZATION_FILES}; do
     KUSTOMIZATION_DIR=$(dirname ${KUSTOMIZATION_FILE})
 
+    if grep "kind: Component" ${KUSTOMIZATION_FILE}
+    then
+      log "${KUSTOMIZATION_DIR} is a Component. Skipping"
+      continue
+    fi
+
     log "Processing kustomization.yaml in ${KUSTOMIZATION_DIR}"
     set_kustomize_load_arg_and_value
     kustomize build "${build_load_arg}" "${build_load_arg_value}" "${KUSTOMIZATION_DIR}" 1> /dev/null
@@ -588,19 +594,25 @@ pgo_dev_deploy() {
 
   kust_file="${build_dir}/cluster-tools/pgo/kustomization.yaml"
   prov_kust_file="${build_dir}/ping-cloud/pingfederate/provisioning/kustomization.yaml"
-  pgo_feature_flag "${kust_file}" "${prov_kust_file}"
+  monitor_kust_file="${build_dir}/cluster-tools/monitoring/pgo/kustomization.yaml"
+  pgo_feature_flag "${kust_file}" "${prov_kust_file}" "${monitor_kust_file}"
 }
 
 # Clear the kustomize file, effectively turning off that block of kustomize code
 pgo_feature_flag() {
   local pgo_kust_file="${1}"
   local prov_kust_file="${2}"
+  local monitor_kust_file="${3}"
 
   if [[ $PF_PROVISIONING_ENABLED != "true" ]]; then
     log "FEATURE FLAG - PF Provisioning is disabled, removing"
     message="# PF_PROVISIONING_ENABLED has been set to 'false', therefore this file has been cleared to disable the feature"
+    component_message="kind: Component
+apiVersion: kustomize.config.k8s.io/v1alpha1
+# PF_PROVISIONING_ENABLED has been set to 'false', therefore this file has been cleared to disable the feature"
     echo "${message}" > "${pgo_kust_file}"
     echo "${message}" > "${prov_kust_file}"
+    echo "${component_message}" > "${monitor_kust_file}"
   fi
 }
 
