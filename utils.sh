3f2613d93607c678bfeced195938c951ab9ab7d4
Jack Miner <jackminer@pingidentity.com>
2022-11-03 16:48:32 +0000

    PDO-4591 Fix IRSA/PGO SSM Issues

--- utils.sh
+++ utils.sh
@@ -677,59 +677,56 @@ apply_crds() {
   fi
 }
 
-# Get the PGO backup bucket name
-get_pgo_backup_bucket_name() {
-  local pgo_backup_env=${1}
-
-  if [[ "${pgo_backup_env}" == "ssm://"* ]]; then
-    if ! ssm_value=$(get_ssm_value "${pgo_backup_env#ssm:/}"); then
-      pgo_backup_env="not_set"
-    else
-      pgo_backup_env="${ssm_value}"
-    fi
-  fi
-
-  echo "${pgo_backup_env#s3://}"
-}
-
 ########################################################################################################################
 # Set a given variable name based on an SSM prefix and suffix. If SSM exists, the ssm_template will
 # be used to set the value. If the SSM prefix is 'unused', no value is set and SSM isn't checked.
 #
 # Arguments
 #   $1 -> var_name - the name of the variable to set
-#   $2 -> ssm_prefix - SSM prefix
-#   $3 -> ssm_suffix - The rest of the SSM key past the prefix
-#   $4 -> ssm_template - A template to render with ${ssm_value} - for example - 'Hello my name is ${ssm_value}' will set
-#                        the variable $var_name to that rendered value
+#   $2 -> var_default - the default value for the variable if SSM is unused or there is an error
+#   $3 -> ssm_prefix - SSM prefix
+#   $4 -> ssm_suffix - The rest of the SSM key past the prefix
+#   $5 -> ssm_template - [OPTIONAL] A template to render with ${ssm_value} - for example -
+#                        'Hello my name is ${ssm_value}' will set the variable $var_name to that rendered value
 ########################################################################################################################
-set_templated_var() {
+set_var() {
   local var_name="${1}"
-  local ssm_prefix="${2}"
-  local ssm_suffix="${3}"
-  local ssm_template="${4}"
+  local var_default="${2}"
+  local ssm_prefix="${3}"
+  local ssm_suffix="${4}"
+  local ssm_template="${5}"
 
-  # Get the actual variable value
-  local var_value="${!1}"
+  # Set a default that will always be returned
+  local var_value="${var_default}"
 
-  if [[ ${var_value} != '' ]]; then
+  # Get the actual variable value from the passed in var string
+  if [[ "${!1}" != '' ]]; then
+    var_value="${!1}"
     echo "${var_name} already set to '${var_value}'"
     return
   elif [[ ${ssm_prefix} != "unused" ]]; then
+    # Remove ssm:/ if provided - all paths should start with '/'
+    if [[ ${ssm_prefix} == *"ssm://"* ]]; then
+      ssm_prefix="${ssm_prefix#ssm:/}"
+    fi
     echo "${var_name} is not set, trying to find it in SSM..."
-    if ! ssm_value=$(get_ssm_value "${ssm_prefix}/${ssm_suffix}"); then
-      echo "WARN: Issue fetching SSM path '${ssm_prefix}/${ssm_suffix}' - ${ssm_value}...
-            Continuing as this could be a disabled environment"
+    if ! ssm_value=$(get_ssm_value "${ssm_prefix}${ssm_suffix}"); then
+      printf '\tWARN: Issue fetching SSM path '%s%s' - %s...\nContinuing as this could be a disabled environment\n' \
+             "${ssm_suffix}" "${ssm_prefix}" "${ssm_value}"
     else
-      echo "Found '${ssm_prefix}/${ssm_suffix}' in SSM"
-      # Substitute ssm_value within the supplied ssm template
-      var_value=$(echo "${ssm_template}" | ssm_value=${ssm_value} envsubst)
+      printf '\tFound "%s%s" in SSM\n' "${ssm_prefix}" "${ssm_suffix}"
+      # Substitute ssm_value within the supplied ssm template, if template given
+      if [ -n "${ssm_template}" ]; then
+        var_value=$(echo "${ssm_template}" | ssm_value=${ssm_value} envsubst)
+      else
+        var_value="${ssm_value}"
+      fi
     fi
   else
-    echo "Not fetching SSM - it is set to 'unused'"
+    printf "%s - not fetching SSM - prefix is set to 'unused'\n" "${var_name}"
   fi
 
   # Always export the variable and value
-  echo "Setting '${var_name}' to '${var_value}'"
+  printf '\tSetting "%s" to "%s"\n' "${var_name}" "${var_value}"
   export "${var_name}=${var_value}"
 }
