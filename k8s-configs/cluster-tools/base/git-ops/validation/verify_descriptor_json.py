a5fcff19e73f6a40c463dfa7b3537514912027bc
vathsalyakidambi <vathsalyakidambi@pingidentity.com>
2023-02-09 17:23:16 -0600

    [skip pipeline]

--- k8s-configs/cluster-tools/base/git-ops/validation/verify_descriptor_json.py
+++ k8s-configs/cluster-tools/base/git-ops/validation/verify_descriptor_json.py
@@ -6,7 +6,6 @@ def verify_descriptor(descriptor_json):
     """Wrapper method that calls all verification methods"""
     verify_json_schema(descriptor_json)
 
-
 def verify_json_schema(descriptor_json):
     """Verify hostname and replicas is included and that replicas is a number."""
     regions = descriptor_json.keys()
@@ -34,9 +33,7 @@ def verify_json_schema(descriptor_json):
     
 if __name__ == "__main__":
     descriptor_json_file_path = sys.argv[1]
-
     # Generate descriptor.json as a dict
     descriptor_json = get_json(descriptor_json_file_path)
     verify_descriptor(descriptor_json)
-
     print("Descriptor.json file is valid")
