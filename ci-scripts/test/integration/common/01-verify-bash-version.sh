2e3c192b1db4355fd0da2ff5b7f290849caffaaf
Hemanth Chebrolu <hchebrolu@pingidentity.com>
2023-02-10 19:26:52 +0530

    [skip pipeline] PDO-4055 added test case to verify installed bash version

---
+++ ci-scripts/test/integration/common/01-verify-bash-version.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. "${PROJECT_DIR}"/ci-scripts/common.sh "${1}"
+
+if skipTest "${0}"; then
+  log "Skipping test ${0}"
+  exit 0
+fi
+
+test_check_bash_version(){
+    #invoke the check_bash_version() by passing a name space we want to check
+    check_bash_version $PING_CLOUD_NAMESPACE
+}
+
+# When arguments are passed to a script you must
+# consume all of them before shunit is invoked
+# or your script won't run.  For integration
+# tests, you need this line.
+shift $#
+
+# load shunit
+. ${SHUNIT_PATH}
