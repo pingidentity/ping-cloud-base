a87b9f2e294b8a842afd8eb074989af54622cb09
Hemanth Reddy <hemanthreddy@pingidentity.com>
2023-04-17 17:58:52 +0000

    Update validate_yaml.py

--- validate_yaml.py
+++ validate_yaml.py
@@ -10,10 +10,11 @@ def repr_str(dumper, data):
         return dumper.represent_scalar("tag:yaml.org,2002:str", fixed_data, style="|")
     return dumper.represent_scalar("tag:yaml.org,2002:str", data)
 
+
 def validate_yaml_file_data():
+
     # Get current working directory
     path = os.getcwd()
-    # path =  "/Users/hemanthreddy/ping-cloud-base/k8s-configs/ping-cloud/test/pingfederate"
     # list out folders and files
     for root, directories, files in os.walk(path):
         # list out yaml and yml extension files
@@ -25,22 +26,25 @@ def validate_yaml_file_data():
                     file_directory = os.path.join(root)
                     yaml_file_contents = Path(file_path).read_text()
                     try:
-                        yaml_object = yaml.full_load(yaml_file_contents)
+                        yaml_object = yaml.safe_load_all(yaml_file_contents)
+                        #  print(yaml_object)
                         try:
                             yaml.add_representer(str, repr_str, Dumper=yaml.SafeDumper)
                             yaml.representer.SafeRepresenter.add_representer(
                                 str, repr_str
                             )
                             with open(file_path, "w") as write:
-                                yaml.safe_dump(yaml_object, write, allow_unicode=True)
+                                yaml.safe_dump_all(
+                                    yaml_object, write, allow_unicode=True
+                                )
                             print("file path is ", os.path.join(root, name))
                         except Exception as e:
                             print(f"No Kind key {file_path}", e)
                     except Exception as e:
                         print(f"invalid yaml {file_path}", e)
+
             except Exception as e:
                 print("yaml file not valid", e)
-
 
 # call method for output
 validate_yaml_file_data()
