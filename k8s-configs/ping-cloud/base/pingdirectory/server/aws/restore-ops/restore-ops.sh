3fdd4fc36a0c78deedb05d112935ee3e25efb8ca
calvincarter <calvincarter@pingidentity.com>
2023-04-19 17:26:17 -0500

    [skip pipeline] adding backend and restore op scripts

---
+++ k8s-configs/ping-cloud/base/pingdirectory/server/aws/restore-ops/restore-ops.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set ping-cloud namespace
+# Note: The regular expression \bping-cloud\S* matches any string that starts with "ping-cloud" (\bping-cloud) and has zero or more non-space characters after it (\S*).
+# e.g.
+# A CDE with ping-cloud namespace will set the variable NAMESPACE as 'ping-cloud'
+# A CDE with ping-cloud-username namespace will set the variable NAMESPACE as 'ping-cloud-username'
+PING_CLOUD_NAMESPACE=$(kubectl get namespaces -o jsonpath='{.items[*].metadata.name}{"\n"}' | grep -o -E "\bping-cloud\S*")
+
+# Get desired PingDirectory pod name
+BACKUP_RESTORE_POD=$(kubectl get configmap pingdirectory-environment-variables -o jsonpath='{.data.BACKUP_RESTORE_POD}' -n "${PING_CLOUD_NAMESPACE}")
+K8S_STATEFUL_SET_NAME=$(kubectl get configmap pingdirectory-environment-variables -o jsonpath='{.data.K8S_STATEFUL_SET_NAME}' -n "${PING_CLOUD_NAMESPACE}")
+test -z "${BACKUP_RESTORE_POD}" && export PINGDIRECTORY_POD_NAME="${K8S_STATEFUL_SET_NAME}-0" || export PINGDIRECTORY_POD_NAME="${BACKUP_RESTORE_POD}"
+
+# Get desired PingDirectory PVC size
+export PINGDIRECTORY_PVC_SIZE=$(kubectl get pvc "out-dir-${PINGDIRECTORY_POD_NAME}" -o jsonpath='{.spec.resources.requests.storage}' -n "${PING_CLOUD_NAMESPACE}")
+
+# TODO leverage wget to get all resources from github
+parent_directory="$(dirname "$(realpath "$0")")"
+cat "${parent_directory}/restore-pvc.yaml" | envsubst | kubectl apply -f -
+cat "${parent_directory}/restore-cm.yaml"  | kubectl apply -f -
+cat "${parent_directory}/restore-job.yaml" | envsubst | kubectl apply -f -
