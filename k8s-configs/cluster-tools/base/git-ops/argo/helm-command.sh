f1d0442ae85a210e2ebb0fe179ab3d13124036d3
Walker <walkerbrown@pingidentity.com>
2023-04-24 15:31:08 -0600

    Adding in custom helm command for ECR

---
+++ k8s-configs/cluster-tools/base/git-ops/argo/helm-command.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ $@ = pull* ]]; then
+    # If the command is `helm pull (..)` skips --repo flag and chartName
+    # from command line args to make helm pull run
+
+    # For explanation:
+    # https://github.com/kubernetes-sigs/kustomize/issues/4381
+    arr=(${@//--repo/});  # Skipping --repo
+    args="${arr[@]:0:5} ${arr[@]:6}";  # Skipping chartName
+else
+    args="$@"
+fi
+cmd="/usr/local/bin/helm --registry-config ~/.config/helm/registry/config.json $args"
+echo "Running '$cmd' " >> /tmp/helm-debug
+eval $cmd
