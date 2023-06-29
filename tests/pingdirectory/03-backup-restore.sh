449e6d3885a8467342c5d89972f63e80d85e65a0
vathsalyakidambi <vathsalyakidambi@pingidentity.com>
2023-06-28 20:49:17 -0500

    [skip pipeline]

--- tests/pingdirectory/03-backup-restore.sh
+++ tests/pingdirectory/03-backup-restore.sh
@@ -4,10 +4,10 @@ CI_SCRIPTS_DIR="${SHARED_CI_SCRIPTS_DIR:-/ci-scripts}"
 . "${CI_SCRIPTS_DIR}"/common.sh "${1}" > /dev/null
 . "${CI_SCRIPTS_DIR}"/test/test_utils.sh
 
-if skipTest "${0}"; then
-  log "Skipping test ${0}"
-  exit 0
-fi
+# if skipTest "${0}"; then
+#   log "Skipping test ${0}"
+#   exit 0
+# fi
 
 expected_files() {
   kubectl logs -n "${PING_CLOUD_NAMESPACE}" \
@@ -20,12 +20,12 @@ expected_files() {
 }
 
 actual_files() {
-  local bucket_url=$(get_ssm_val "${BACKUP_URL#ssm:/}")
-  local bucket_url_no_protocol=${bucket_url#s3://}
+  BUCKET_URL_NO_PROTOCOL=${BACKUP_URL#s3://}
+  BUCKET_NAME=$(echo "${BUCKET_URL_NO_PROTOCOL}" | cut -d/ -f1)
   DAYS_AGO=1
 
   aws s3api list-objects \
-    --bucket "${bucket_url_no_protocol}" \
+    --bucket "${BUCKET_NAME}" \
     --prefix 'pingdirectory/' \
     --query "reverse(sort_by(Contents[?LastModified>='${DAYS_AGO}'], &LastModified))[].Key" \
     --profile "${AWS_PROFILE}" |
@@ -36,12 +36,15 @@ actual_files() {
 
 testBackupAndRestore() {
 
-  BACKUP_JOB="${PROJECT_DIR}"/k8s-configs/ping-cloud/base/pingdirectory/server/aws/backup.yaml
+  BACKUP_OPS_SCRIPT="${PROJECT_DIR}"/k8s-configs/ping-cloud/base/pingdirectory/server/aws/backup-ops-template/backup-ops.sh
+  #BACKUP_OPS_SCRIPT="/Users/vathsalyakidambi/Desktop/repos/ping-cloud-base/k8s-configs/ping-cloud/base/pingdirectory/server/aws/backup-ops-template/backup-ops.sh"
+  BACKUP_JOB="pingdirectory-backup"
 
   log "Applying the backup job"
-  kubectl delete -f "${BACKUP_JOB}" -n "${PING_CLOUD_NAMESPACE}"
+  #kubectl delete -f "${BACKUP_JOB}" -n "${PING_CLOUD_NAMESPACE}"
+  test -x "${BACKUP_OPS_SCRIPT}" && "${BACKUP_OPS_SCRIPT}"
 
-  kubectl apply -f "${BACKUP_JOB}" -n "${PING_CLOUD_NAMESPACE}"
+  kubectl get job "${BACKUP_JOB}" -n "${PING_CLOUD_NAMESPACE}"
   assertEquals "The kubectl apply command to create the ${BACKUP_JOB} should have succeeded" 0 $?
 
   log "Waiting for the backup job to complete"
@@ -62,11 +65,14 @@ testBackupAndRestore() {
     exit 1
   fi
 
-  RESTORE_JOB="${PROJECT_DIR}"/k8s-configs/ping-cloud/base/pingdirectory/server/aws/restore.yaml
+  RESTORE_OPS_SCRIPT="/Users/vathsalyakidambi/Desktop/repos/ping-cloud-base/k8s-configs/ping-cloud/base/pingdirectory/server/aws/restore-ops-template/restore-ops.sh"
+  RESTORE_JOB="pingdirectory-restore"
 
   log "Applying the restore job"
 #   kubectl delete -f "${RESTORE_JOB}" -n "${PING_CLOUD_NAMESPACE}"
-  kubectl apply -f "${RESTORE_JOB}" -n "${PING_CLOUD_NAMESPACE}"
+  test -x "${RESTORE_OPS_SCRIPT}" && "${RESTORE_OPS_SCRIPT}"
+
+  kubectl get job "${RESTORE_JOB}" -n "${PING_CLOUD_NAMESPACE}"
   assertEquals "The kubectl apply command to create the ${RESTORE_JOB} should have succeeded" 0 $?
 
   log "Waiting for the restore job to complete"
@@ -74,9 +80,9 @@ testBackupAndRestore() {
   assertEquals "The kubectl wait command for the job should have succeeded" 0 $?
 
   # We expect 3 backends to be restored successfully
-  RESTORE_SUCCESS_MESSAGE='Restore task .* has been successfully completed'
+  RESTORE_SUCCESS_MESSAGE='The restore process completed successfully'
   RESTORE_POD=$(kubectl get pod -n "${PING_CLOUD_NAMESPACE}" -o name | grep pingdirectory-restore)
-  NUM_SUCCESSFUL=$(kubectl logs -n "${PING_CLOUD_NAMESPACE}" "${RESTORE_POD}" | grep -o "${RESTORE_SUCCESS_MESSAGE}" | sort -u | wc -l )
+  NUM_SUCCESSFUL=$(kubectl logs -n "${PING_CLOUD_NAMESPACE}" "${RESTORE_POD}" | grep -o "${RESTORE_SUCCESS_MESSAGE}" | wc -l )
 
   NUM_EXPECTED=3
   assertEquals "Restore job failed" "${NUM_EXPECTED}" "${NUM_SUCCESSFUL}"
@@ -86,6 +92,7 @@ testBackupAndRestore() {
   fi
 }
 
+#testBackupAndRestore
 
 # When arguments are passed to a script you must
 # consume all of them before shunit is invoked
