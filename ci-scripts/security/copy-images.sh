111b3eb12cfd94cf5d32d02ebf27018c7e1f4696
Geoff Lancaster <geofflancaster@pingidentity.com>
2023-06-30 13:29:24 -0500

    providing docker login pw via stdin and changing docker home

--- ci-scripts/security/copy-images.sh
+++ ci-scripts/security/copy-images.sh
@@ -4,13 +4,17 @@ set -e
 
 test "${VERBOSE}" && set -x
 
+function docker_command() {
+  HOME=/tmp docker "${@:2}"
+}
+
 # Source common environment variables
 SCRIPT_HOME=$(cd $(dirname ${0}); pwd)
 . ${SCRIPT_HOME}/../common.sh
 
 # login first so we can error immediately if this isn't going to work
 # may also need to login to ECR or other repos that we need to pull images from before pushing
-docker login $ARTIFACTORY_URL -u $ARTIFACTORY_REGISTRY_USER -p $ARTIFACTORY_REGISTRY_PW
+cat $ARTIFACTORY_REGISTRY_PW | docker_command login $ARTIFACTORY_URL -u $ARTIFACTORY_REGISTRY_USER --password-stdin
 
 deploy_file=/tmp/deploy.yaml
 build_dev_deploy_file "${deploy_file}"
@@ -25,9 +29,9 @@ for image in $(cat $deploy_file | grep "image:" | awk -F: 'BEGIN { OFS=":"} {pri
   else                                                                                                            # dockerhub images without domain
     name=$image
   fi
-  
-  docker pull $image
-  docker tag $image $ARTIFACTORY_URL/$BELUGA_VERSION/$name
-  docker push $ARTIFACTORY_URL/$BELUGA_VERSION/$name
+
+  docker_command pull $image
+  docker_command tag $image $ARTIFACTORY_URL/$BELUGA_VERSION/$name
+  docker_command push $ARTIFACTORY_URL/$BELUGA_VERSION/$name
   echo "Copied $image to location $ARTIFACTORY_URL/$BELUGA_VERSION/$name"
 done
