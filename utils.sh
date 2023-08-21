432d73fb2630c26ea9e4d17f569f12568cbf0c56
abhalla <abhalla@pingidentity.com>
2023-08-21 10:21:59 +0530

    Adding changes

--- utils.sh
+++ utils.sh
@@ -123,27 +123,6 @@ parse_url() {
 }
 
 ########################################################################################################################
-# Format the provided kustomize version for numeric comparison. For example, if the kustomize version is 4.0.5, it
-# returns 004000005000.
-#
-# Arguments
-#   ${1} -> The kustomize short version, e.g. v4.0.5.
-########################################################################################################################
-format_version() {
-  version="$1"
-  printf "%03d%03d%03d%03d" $(echo "${version}" | tr '.' ' ')
-}
-
-########################################################################################################################
-# Returns the version of kustomize formatted for numeric comparison. For example, if the kustomize version is 4.0.5,
-# it returns 004000005000.
-########################################################################################################################
-kustomize_version() {
-  version="$(kustomize version --short | grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+')"
-  format_version "${version}"
-}
-
-########################################################################################################################
 # Substitute variables in all files in the provided directory.
 #
 # Arguments
@@ -160,8 +139,8 @@ ${ENVIRONMENT}
 ${BELUGA_ENV_NAME}
 ${IS_MULTI_CLUSTER}
 ${PLATFORM_EVENT_QUEUE_NAME}
+${CUSTOMER_SSM_PATH_PREFIX}
 ${CUSTOMER_SSO_SSM_PATH_PREFIX}
-${ORCH_API_SSM_PATH_PREFIX}
 ${SERVICE_SSM_PATH_PREFIX}
 ${REGION}
 ${REGION_NICK_NAME}
