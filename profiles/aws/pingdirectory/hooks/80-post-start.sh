c4718d9030bea3f3e532c75d71f37129c8109d96
calvincarter <calvincarter@pingidentity.com>
2023-08-26 23:14:18 -0500

    [skip pipeline] cleanup

--- profiles/aws/pingdirectory/hooks/80-post-start.sh
+++ profiles/aws/pingdirectory/hooks/80-post-start.sh
@@ -122,39 +122,6 @@ initialize_server_for_dn() {
   return ${replInitResult}
 }
 
-########################################################################################################################
-# Initialize replication for all servers for provided base DN from the primary seed server.
-#
-# Arguments
-#   ${1} -> The base DN for which to initialize replication.
-########################################################################################################################
-initialize_all_servers_for_dn() {
-  BASE_DN=${1}
-
-  if is_multi_cluster; then
-    beluga_log "waiting for the replication seed server ${PD_SEED_LDAP_HOST}:${PD_LDAPS_PORT}"
-    waitUntilLdapUp "${PD_SEED_LDAP_HOST}" "${PD_LDAPS_PORT}" 'cn=config'
-  fi
-
-  beluga_log "running dsreplication initialize-all for ${BASE_DN} from primary seed server"
-  dsreplication initialize-all \
-    --hostname "${PD_SEED_LDAP_HOST}" --port "${PD_LDAPS_PORT}" \
-    --retryTimeoutSeconds "${RETRY_TIMEOUT_SECONDS}" \
-    --baseDN "${BASE_DN}" \
-    --no-prompt --ignoreWarnings \
-    --enableDebug \
-    --globalDebugLevel verbose
-  replInitResult=$?
-  beluga_log "replication initialize-all for ${BASE_DN} status: ${replInitResult}"
-
-  if test ${replInitResult} -ne 0; then
-    beluga_log "contents of dsreplication.log:"
-    cat "${SERVER_ROOT_DIR}"/logs/tools/dsreplication.log
-  fi
-
-  return ${replInitResult}
-}
-
 capture_latest_logs() {
   msg="${1}"
   status_code=${2}
@@ -226,42 +193,22 @@ if ! "${INITIALIZE_REPLICATION_DATA}"; then
   exit 0
 fi
 
-if test "${ORDINAL}" -eq 0 && is_primary_cluster; then
-  beluga_log "seed server in the primary cluster"
-
-  if test ! -f "${REPL_INIT_MARKER_FILE}"; then
-    # As the other servers come up, they'll initialize replication data from another server.
-    # So we can skip replication here.
-    beluga_log "initial launch - all DNs will be initialized by each server as they come up"
-    for DN in ${UNINITIALIZED_DNS}; do
-      echo "${DN}" >> "${REPL_INIT_MARKER_FILE}"
-    done
-
-    exit 0
-  else
-    # If the USER_BASE_DN changes (e.g. from dc=example,dc=com to dc=refinitiv,dc=com), then
-    # replication data must be initialized to all servers from the primary server. The individual
-    # servers cannot initialize themselves because the rollout happens in reverse order, and the
-    # user base DN will not exist on the servers that haven't been rolled out yet.
-    beluga_log "restart - data will be initialized from seed server to all servers for base DNs: ${UNINITIALIZED_DNS}"
-    command_to_run="initialize_all_servers_for_dn"
-  fi
-else
+if is_first_time_deploy_child_server; then
   beluga_log "non-seed server - will initialize data from another server"
+
   command_to_run="initialize_server_for_dn"
+  beluga_log "replication will be initialized for base DNs: ${UNINITIALIZED_DNS}"
+  for DN in ${UNINITIALIZED_DNS}; do
+    ${command_to_run} "${DN}"
+    replInitResult=$?
+
+    if test ${replInitResult} -eq 0; then
+      beluga_log "adding DN ${DN} to the replication marker file ${REPL_INIT_MARKER_FILE}"
+      echo "${DN}" >> "${REPL_INIT_MARKER_FILE}"
+    else
+      exit 1
+    fi
+  done
 fi
-
-beluga_log "replication will be initialized for base DNs: ${UNINITIALIZED_DNS}"
-for DN in ${UNINITIALIZED_DNS}; do
-  ${command_to_run} "${DN}"
-  replInitResult=$?
-
-  if test ${replInitResult} -eq 0; then
-    beluga_log "adding DN ${DN} to the replication marker file ${REPL_INIT_MARKER_FILE}"
-    echo "${DN}" >> "${REPL_INIT_MARKER_FILE}"
-  else
-    exit 1
-  fi
-done
 
 exit 0
