2ecd4c716ce6509736f4969c8cc00c4afefa346d
Colin Reininger <colinreininger@pingidentity.com>
2023-10-02 16:25:48 -0600

    Add argocd sso app integration test

--- tests/pingone/test_argo_sso.py
+++ tests/pingone/test_argo_sso.py
@@ -11,9 +11,14 @@ class TestArgoSSO(p1_test_base.P1TestBase):
             f"{self.tenant_name}-argo-config-team",
             "argo-platform",
         ]
+        self.app_name = f"client-{self.tenant_name}-{self.environment_name}-argo-sso"
 
     def test_groups_created(self):
         for group_name in self.group_names:
             with self.subTest(msg=f"{group_name} created"):
                 p1_group = self.get(self.cluster_env_endpoints.groups, group_name)
                 self.assertIsNotNone(p1_group, f"Group '{group_name}' not created")
+
+    def test_app_created(self):
+        p1_app = self.get(self.cluster_env_endpoints.applications, self.app_name)
+        self.assertIsNotNone(p1_app, f"Application '{self.app_name}' not created")
