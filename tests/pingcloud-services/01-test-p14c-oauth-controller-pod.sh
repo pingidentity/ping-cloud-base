8cf1fda214ba48703e88e5e198452c488dc912c1
Kanesha Fox <kaneshafox@pingidentity.com>
2023-10-24 19:54:21 +0000

    PDO-5146 "Add p1 create cluster"

--- tests/pingcloud-services/01-test-p14c-oauth-controller-pod.sh
+++ tests/pingcloud-services/01-test-p14c-oauth-controller-pod.sh
@@ -9,7 +9,7 @@ if skipTest "${0}"; then
 fi
 
 testP14COAuthPodAvailability() {
-  status=$(kubectl get pods --selector=role=p14c-oauth-service -n ${PING_CLOUD_NAMESPACE} -o json | jq -r '.items[].status.phase')
+  status=$(kubectl get pods --selector=job-name=p14c-oauth-service -n ${PING_CLOUD_NAMESPACE} -o json | jq -r '.items[].status.phase')
   assertEquals 0 $?
   # There may be multiple pods from failed jobs, but if at least one succeeds we're good
   assertContains "The status phase of the p14c-oauth-service pod should be Succeeded but was: ${status}" "${status}" "Succeeded"
