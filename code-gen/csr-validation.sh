6d081115974e2f11466a423190f96a9453343006
abhalla <abhalla@pingidentity.com>
2023-11-03 09:59:23 +0530

    Adding change log

--- code-gen/csr-validation.sh
+++ code-gen/csr-validation.sh
@@ -132,6 +132,7 @@ for app_path in ${app_region_paths}; do
     full_out_dir="${OUT_DIR}/${app_path#./}"
     mkdir -p "${full_out_dir}"
     result=$( (kustomize build --load-restrictor LoadRestrictionsNone --enable-helm --output "${full_out_dir}/uber.yaml" "${app_path}" ) 2>&1)
+    cp "${full_out_dir}/uber.yaml" "${CI_PROJECT_DIR}"
   fi
   # if kustomize build fails: add to failure list and output the error
   # note: this check needs to be immediately after the above "results=" command so that it can check the exit code
