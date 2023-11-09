011fe217cb31ce3c21a8c053d37fa81d77d9cfbb
Walker <walkerbrown@pingidentity.com>
2023-11-09 16:14:26 -0700

    Sourcing env vars

--- tests/pre-check/cicd_pre_check.py
+++ tests/pre-check/cicd_pre_check.py
@@ -1,10 +1,17 @@
 import unittest
 import requests
 from os import getenv
+from dotenv import load_dotenv
+
+CI_SCRIPTS_DIR = getenv("SHARED_CI_SCRIPTS_DIR")
 
 class CicdPreCheckTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        load_dotenv(f"{CI_SCRIPTS_DIR}/k8s/deploy/ci-cd-cluster.properties")
+
     def test_ingress(self):
-        dns_zone = getenv("DNS_ZONE", "ping-demo.com")
+        dns_zone = getenv("PRIMARY_TENANT_DOMAIN", "ping-demo.com")
         subdomains = [
             "healthcheck",
             "metadata"
