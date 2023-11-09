e205a0f9f299be6c5acec917d674da26f5d02c75
Walker <walkerbrown@pingidentity.com>
2023-11-09 15:34:11 -0700

    Removing external URLs

--- tests/pre-check/cicd_pre_check.py
+++ tests/pre-check/cicd_pre_check.py
@@ -2,26 +2,17 @@ import unittest
 import requests
 from os import getenv
 
-class CicdPreCheckTests(unittest.testcase):
+class CicdPreCheckTests(unittest.TestCase):
     def test_ingress(self):
         dns_zone = getenv("DNS_ZONE", "ping-demo.com")
         subdomains = [
             "healthcheck",
-            "metadata",
-            "pingaccess-admin-api",
-            "pingaccess-agent",
-            "pingaccess",
-            "pingaccess-was-admin"
-            "pingaccess-admin",
-            "pingdelegator",
-            "pingdirectory",
-            "pingfederate-admin-api",
-            "pingfederate"
+            "metadata"
         ]
         for subdomain in subdomains:
             url = f"https://{subdomain}.{dns_zone}"
             try:
-                response = requests.get(url)
+                response = requests.get(url, verify=False)
                 response.raise_for_status()
             except requests.exceptions.RequestException as e:
                 self.fail(f"Failed to make GET request to {url}: {e}")
