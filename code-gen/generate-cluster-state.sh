e3779a37f5caed3f90707b01c1c06eaabc825724
Hemanth Chebrolu <hchebrolu@pingidentity.com>
2023-11-28 17:25:10 +0000

    PDO-6323 Add duration and renewBefore properties within cert-manager

--- code-gen/generate-cluster-state.sh
+++ code-gen/generate-cluster-state.sh
@@ -454,7 +454,8 @@ ${PROM_SLACK_CHANNEL}
 ${DASH_REPO_URL}
 ${DASH_REPO_BRANCH}
 ${APP_RESYNC_SECONDS}
-${IMAGE_LIST}'
+${IMAGE_LIST}
+${CERT_RENEW_BEFORE}'
 
 # Variables to replace within the generated cluster state code
 REPO_VARS="${REPO_VARS:-${DEFAULT_VARS}}"
@@ -783,6 +784,8 @@ echo "Initial APP_RESYNC_SECONDS: ${APP_RESYNC_SECONDS}"
 
 echo "Initial DASHBOARD_REPO_URL: ${DASHBOARD_REPO_URL}"
 
+echo "Initial CERT_RENEW_BEFORE: ${CERT_RENEW_BEFORE}"
+
 echo ---
 
 
@@ -999,6 +1002,8 @@ export PA_GCOPTION='-XX:+UseParallelGC'
 
 export APP_RESYNC_SECONDS="${APP_RESYNC_SECONDS:-60}"
 
+export CERT_RENEW_BEFORE="${CERT_RENEW_BEFORE:-720h}"
+
 ########################################################################################################################
 # Print out the final value being used for each variable.
 ########################################################################################################################
@@ -1083,6 +1088,8 @@ echo "Using IMAGE_LIST: ${IMAGE_LIST}"
 echo "Using IMAGE_TAG_PREFIX: ${IMAGE_TAG_PREFIX}"
 
 echo "Using DASHBOARD_REPO_URL: ${DASHBOARD_REPO_URL}"
+
+echo "Using CERT_RENEW_BEFORE: ${CERT_RENEW_BEFORE}"
 
 echo ---
 
