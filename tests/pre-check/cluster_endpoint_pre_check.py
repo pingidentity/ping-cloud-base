ad05f55144903a4e866dce1de264bef6f31bb7f0
Kanesha Fox <kaneshafox@pingidentity.com>
2024-11-06 21:16:09 +0000

    cherry-pick-c6f0fcdc_to_v2.1-release-branch_PDO-8416

--- tests/pre-check/cluster_endpoint_pre_check.py
+++ tests/pre-check/cluster_endpoint_pre_check.py
@@ -1,25 +1,38 @@
+import os
 import unittest
+import warnings
+
 import requests
 from os import getenv
-from tenacity import retry, stop_after_attempt, wait_fixed
 
-SUBDOMAINS = ["healthcheck", "metadata"]
+import urllib3
+from tenacity import retry, stop_after_attempt, wait_fixed
 
 
 class ClusterEndpointsPreCheck(unittest.TestCase):
-    def test_ingress(self):
+    @classmethod
+    def setUpClass(cls) -> None:
+        # Ignore warnings for insecure http requests
+        warnings.filterwarnings(
+            "ignore", category=urllib3.exceptions.InsecureRequestWarning
+        )
         tenant_domain = getenv("PRIMARY_TENANT_DOMAIN", "ping-oasis.com")
+        cls.domains = [f"metadata.{tenant_domain}", f"self-service-api.{tenant_domain}/docs"]
 
-        for subdomain in SUBDOMAINS:
-            with self.subTest(msg=f"{subdomain}.{tenant_domain} is not available"):
-                response = self.get_ingress_response(subdomain, tenant_domain)
+        # Add optional domains
+        if os.getenv("HEALTHCHECKS_ENABLED") == "true":
+            cls.domains.append(f"healthcheck.{tenant_domain}")
+
+    def test_ingress(self):
+        for domain in self.domains:
+            with self.subTest(msg=f"{domain} is not available"):
+                response = self.get_ingress_response(f"{domain}")
                 self.assertEqual(response.status_code, 200)
 
     @retry(stop=stop_after_attempt(3), wait=wait_fixed(5))
-    def get_ingress_response(self, subdomain, tenant_domain):
-        url = f"https://{subdomain}.{tenant_domain}"
+    def get_ingress_response(self, url_base):
+        url = f"https://{url_base}"
         print(f"Checking URL: {url}")
-        response = None
         response = requests.get(url, verify=False, timeout=5)
         response.raise_for_status()        
         return response
