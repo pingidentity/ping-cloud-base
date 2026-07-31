356ed60d382518b6197c7aa8a197e9ee1ed91681
Kanesha Fox <kaneshafox@pingidentity.com>
2026-07-31 17:24:34 -0400

    [cherry-pick] PDO-11013: PCB GitHub migration (#3033) (#3036)
    Co-authored-by: Jack Miner <jackminer@pingidentity.com>

--- tests/common/test_seal_script.py
+++ tests/common/test_seal_script.py
@@ -5,7 +5,7 @@ import yaml
 import tempfile
 import shutil
 
-PCB_DIR = os.getenv("PROJECT_DIR", os.getenv("PCB_PATH", "ping-cloud-base"))
+PCB_DIR = os.getenv("PROJECT_DIR", os.getenv("PCB_PATH", "p1as-eng-base"))
 SEAL_SCRIPT_PATH = os.getenv("SEAL_SCRIPT", ("%s/code-gen/seal-secret-values.py" % PCB_DIR))
 VALUES_FILE_PATH = "values-files/base"
 
